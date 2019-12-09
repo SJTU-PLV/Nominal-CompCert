92324a00b4941b49558f78c6657884badd5388e8
Jeremie Koenig <jeremie.koenig@yale.edu>
2019-12-08 19:52:34 -0500

    Update Renumberproof

--- backend/Renumberproof.v
+++ backend/Renumberproof.v
@@ -234,12 +234,10 @@ End PRESERVATION.
 
 Theorem transf_program_correct prog tprog:
   match_prog prog tprog ->
-  open_fsim cc_id cc_id (RTL.semantics prog) (RTL.semantics tprog).
+  forward_simulation cc_id cc_id (RTL.semantics prog) (RTL.semantics tprog).
 Proof.
-  intros MATCH. split; [apply match_program_skel in MATCH; auto | ].
-  intros [ ] se _ q _ _ _ [ ] [ ].
-  split. { eapply Genv.is_internal_transf_id; eauto. destruct fd; auto. }
-  eapply forward_simulation_step; simpl.
+  fsim eapply forward_simulation_step; cbn in *; intros; subst.
+  - eapply Genv.is_internal_transf_id; eauto. destruct fd; auto.
   - eauto using transf_initial_states.
   - eauto using transf_final_states.
   - intros. edestruct transf_external; eauto. exists tt, q1. intuition subst; eauto.
