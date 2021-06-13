d0573da786df99ffa7b94ae70d454fa58551af23
Yu-zh <yu.zhang.yz862@yale.edu>
2021-06-13 09:18:09 +0800

    tweaked semantics and associativity proof

---
+++ common/CategoricalComp.v
@@ -0,0 +1,250 @@
+Require Import Relations.
+Require Import List.
+Require Import Coqlib.
+Require Import LanguageInterface Events Globalenvs SmallstepLinking Smallstep_.
+
+Open Scope smallstep_scope.
+Delimit Scope smallstep_scope with smallstep.
+
+Section COMP.
+  Context {liA liB liC S1 S2} (L1: lts liB liC S1) (L2: lts liA liB S2).
+
+  Variant comp_state := | st1 (s: S1) | st2 (s1: S1) (s2: S2).
+
+  Inductive comp_step: comp_state -> trace -> comp_state -> Prop :=
+  | step1 s1 t s1':
+      Step L1 s1 t s1' ->
+      comp_step (st1 s1) t (st1 s1')
+  | step2 s1 s2 t s2':
+      Step L2 s2 t s2' ->
+      comp_step (st2 s1 s2) t (st2 s1 s2')
+  | step_push s1 q s2:
+      at_external L1 s1 q ->
+      initial_state L2 q s2 ->
+      comp_step (st1 s1) E0 (st2 s1 s2)
+  | step_pop s1 r s2 s1':
+      final_state L2 s2 r ->
+      after_external L1 s1 r s1' ->
+      comp_step (st2 s1 s2) E0 (st1 s1').
+
+  Inductive comp_initial_state (q: query liC): comp_state -> Prop :=
+  | comp_initial_state_intro s:
+      initial_state L1 q s ->
+      (* and the there exists the next step for s *)
+      comp_initial_state q (st1 s).
+
+  Inductive comp_at_external: comp_state -> query liA -> Prop :=
+  | comp_at_external_intro s1 s2 (q: query liA):
+      at_external L2 s2 q ->
+      comp_at_external (st2 s1 s2) q.
+
+  Inductive comp_after_external: comp_state -> reply liA -> comp_state -> Prop :=
+  | comp_after_external_intro s1 s2 r s2':
+      after_external L2 s2 r s2' ->
+      comp_after_external (st2 s1 s2) r (st2 s1 s2').
+
+  Inductive comp_final_state: comp_state -> reply liC -> Prop :=
+  | comp_final_state_intro s r:
+      final_state L1 s r ->
+      comp_final_state (st1 s) r.
+
+  Definition comp_lts :=
+    {|
+    step ge := comp_step;
+    valid_query q := valid_query L1 q || valid_query L2 q;
+    initial_state := comp_initial_state;
+    at_external := comp_at_external;
+    after_external := comp_after_external;
+    final_state := comp_final_state;
+    globalenv := tt;
+    |}.
+
+  (* Definition comp_semantics sk: semantics liA liC := *)
+  (*   {| *)
+  (*   activate se := *)
+  (*     {| *)
+  (*       step ge := comp_step se; *)
+  (*       valid_query q := valid_query (L1 se) q || valid_query (L2 se) q; *)
+  (*       initial_state := comp_initial_state se; *)
+  (*       at_external := comp_at_external se; *)
+  (*       after_external := comp_after_external se; *)
+  (*       final_state := comp_final_state se; *)
+  (*       globalenv := tt; *)
+  (*     |}; *)
+  (*   skel := sk; *)
+  (*   |}. *)
+End COMP.
+
+Section ID.
+  Context {li: language_interface}.
+  Variant id_state :=
+  | st_q (q: query li)
+  | st_r (r: reply li).
+
+  Inductive id_step: id_state -> trace -> id_state -> Prop := .
+
+  Inductive id_initial_state: query li -> id_state -> Prop :=
+  | id_initial_state_intro q:
+      id_initial_state q (st_q q).
+
+  Inductive id_at_external: id_state -> query li -> Prop :=
+  | id_at_external_intro q:
+      id_at_external (st_q q) q.
+
+  Inductive id_after_external: id_state -> reply li -> id_state -> Prop :=
+  | id_after_external_intro q r:
+      id_after_external (st_q q) r (st_r r).
+
+  Inductive id_final_state: id_state -> reply li -> Prop :=
+  | id_final_state_intro r:
+      id_final_state (st_r r) r.
+
+  Definition id_lts :=
+    {|
+    step ge := id_step;
+    valid_query q := false;
+    initial_state := id_initial_state;
+    at_external := id_at_external;
+    after_external := id_after_external;
+    final_state := id_final_state;
+    globalenv := tt;
+    |}.
+  (* Definition id_semantics sk: semantics li li := *)
+  (*   {| *)
+  (*   activate se := *)
+  (*     {| *)
+  (*       step ge := id_step; *)
+  (*       valid_query q := false; *)
+  (*       initial_state := id_initial_state; *)
+  (*       at_external := id_at_external; *)
+  (*       after_external := id_after_external; *)
+  (*       final_state := id_final_state; *)
+  (*       globalenv := tt; *)
+  (*     |}; *)
+  (*   skel := sk; *)
+  (*   |}. *)
+End ID.
+
+Definition fsim_lts {liA liB S1 S2} (L1: lts liA liB S1) (L2: lts liA liB S2) :=
+  exists index order ms, forall se,fsim_properties 1 1 se se tt L1 L2 index order ms.
+Definition equiv_lts {liA liB S1 S2} (L1: lts liA liB S1) (L2: lts liA liB S2) :=
+  fsim_lts L1 L2 /\ fsim_lts L2 L1.
+
+Lemma fsim_lts_trivial_order {liA liB S1 S2} (L1: lts liA liB S1) (L2: lts liA liB S2):
+  (exists ms, forall se, fsim_properties 1 1 se se tt L1 L2 S1 (ltof _ (fun _ => O)) ms) -> fsim_lts L1 L2.
+Proof.
+  intros H. exists S1, (ltof _ (fun _ => O)). apply H.
+Qed.
+
+(* Definition forward_simulation' {liA liB} (L1 L2: semantics liA liB) := *)
+(*   forward_simulation 1 1 L1 L2. *)
+(* Definition equiv_simulation {liA liB} (L1 L2: semantics liA liB) := *)
+(*   forward_simulation' L1 L2 /\ forward_simulation' L2 L1. *)
+
+Notation "L1 ≤ L2" :=  (fsim_lts L1 L2)(at level 90): lts_scope.
+Notation "L1 ≡ L2" :=  (equiv_lts L1 L2)(at level 90): lts_scope.
+Notation "L1 ∘ L2" :=  (comp_lts L1 L2)(at level 50, left associativity): lts_scope.
+Notation " 1 " :=  (id_lts): lts_scope.
+Open Scope lts_scope.
+Delimit Scope lts_scope with lts.
+
+Section IDENTITY.
+  Context {liA liB S} (L : lts liA liB S).
+
+  Inductive id_state_match1: @comp_state id_state S -> S -> Prop :=
+  | id_state_match_query q s:
+      initial_state L q s ->
+      id_state_match1 (st1 (st_q q)) s
+  | id_state_match_state q s:
+      id_state_match1 (st2 (st_q q) s) s
+  | id_state_match_reply r s:
+      id_state_match1 (st1 (st_r r)) s.
+
+  Lemma identity1: 1 ∘ L ≤ L.
+  Proof.
+    eexists _, _, _. intros se.
+    eapply forward_simulation_star_wf with (match_states := id_state_match1).
+    - intros q _ [ ]. now cbn.
+    - intros q _ s1 [ ] H. inv H. inv H0.
+      (* HELP: how do we know that the query has a well defined initial state *)
+  Admitted.
+
+
+  Theorem categorical_comp_left_identity: 1 ∘ L ≡ L.
+  Admitted.
+
+  Theorem categorical_comp_right_identity: L ∘ 1 ≡ L.
+  Admitted.
+
+End IDENTITY.
+
+Section ASSOC.
+
+  Context {liA liB liC liD S1 S2 S3}
+          (L1: lts liC liD S1) (L2: lts liB liC S2) (L3: lts liA liB S3).
+
+  Inductive assoc_state_match:
+    @comp_state S1 (@comp_state S2 S3) -> @comp_state (@comp_state S1 S2) S3 -> Prop :=
+  | assoc_match1 s1: assoc_state_match (st1 s1) (st1 (st1 s1))
+  | assoc_match2 s1 s2: assoc_state_match (st2 s1 (st1 s2)) (st1 (st2 s1 s2))
+  | assoc_match3 s1 s2 s3: assoc_state_match (st2 s1 (st2 s2 s3)) (st2 (st2 s1 s2) s3).
+
+  Lemma assoc1: L1 ∘ (L2 ∘ L3) ≤ L1 ∘ L2 ∘ L3.
+  Proof.
+    eexists. intros se.
+    eapply forward_simulation_step with (match_states := assoc_state_match).
+    - intros. inv H. cbn. now rewrite orb_assoc.
+    - intros q _ s1 [ ] H. inv H.
+      eexists. split; try repeat constructor; auto.
+    - intros s1 s2 r Hs H. inv H. inv Hs.
+      eexists. split; try repeat constructor; auto.
+    - intros s1 s2 q Hs Hext. inv Hext. inv H. inv Hs.
+      eexists tt, _. repeat apply conj; try repeat constructor; auto.
+      intros r _ s1' [ ] Haft. inv Haft. inv H4.
+      eexists. split; try repeat constructor; auto.
+    - intros s1 t s1' Hstep s2 Hs.
+      inv Hstep.
+      + inv Hs. eexists; split; try repeat constructor; auto.
+      + inv H; inv Hs; eexists; (split; [ | constructor ]).
+        * apply step1. now apply step2.
+        * now apply step2.
+        * eapply step_push; try constructor; eauto.
+        * eapply step_pop; try constructor; eauto.
+      + inv H0. eexists; split; [ | constructor ].
+        inv Hs. constructor. eapply step_push; eauto.
+      + inv H. eexists; split; [ | constructor].
+        inv Hs. constructor. econstructor; eauto.
+  Qed.
+
+  Lemma assoc2: L1 ∘ L2 ∘ L3 ≤ L1 ∘ (L2 ∘ L3).
+  Proof.
+    eexists. intros se.
+    eapply forward_simulation_step with (match_states := fun s1 s2 => assoc_state_match s2 s1).
+    - intros. inv H. cbn. now rewrite <- orb_assoc.
+    - intros q _ s1 [ ] H. inv H. inv H0.
+      eexists; split; constructor; auto.
+    - intros s1 s2 r Hs H. inv H. inv H0. inv Hs.
+      eexists; split; constructor; auto.
+    - intros s1 s2 q Hs Hext. inv Hext. inv Hs.
+      eexists tt, _. repeat apply conj; try repeat constructor; auto.
+      intros r _ s1' [ ] Haft. inv Haft.
+      eexists; split; repeat constructor; auto.
+    - intros s1 t s1' Hstep s2 Hs.
+      inv Hstep.
+      + inv H; inv Hs; eexists; (split; [ | constructor ]).
+        * now apply step1.
+        * apply step2. now apply step1.
+        * eapply step_push; try constructor; eauto.
+        * eapply step_pop; try constructor; eauto.
+      + inv Hs. eexists; split; repeat constructor; auto.
+      + inv H. inv Hs. eexists; split; constructor.
+        eapply step_push; eauto.
+      + inv H0. inv Hs. eexists; split; constructor.
+        eapply step_pop; eauto.
+  Qed.
+
+  Theorem categorical_comp_assoc: L1 ∘ (L2 ∘ L3) ≡ L1 ∘ L2 ∘ L3.
+  Proof.
+    split; [ exact assoc1 | exact assoc2 ].
+  Qed.
+End ASSOC.
---
+++ common/Smallstep_.v
@@ -0,0 +1,215 @@
+Require Import Relations.
+Require Import Wellfounded.
+Require Import Coqlib.
+Require Import Events.
+Require Import Globalenvs.
+Require Import LanguageInterface.
+Require Import Integers.
+Require Import Smallstep.
+
+Set Implicit Arguments.
+
+Axiom query_to_identifier: forall li, query li -> string.
+Coercion query_to_identifier: query  >-> string.
+
+Record lts liA liB state: Type := {
+  genvtype: Type;
+  step : genvtype -> state -> trace -> state -> Prop;
+  valid_query: string -> bool;
+  initial_state: query liB -> state -> Prop;
+  at_external: state -> query liA -> Prop;
+  after_external: state -> reply liA -> state -> Prop;
+  final_state: state -> reply liB -> Prop;
+  globalenv: genvtype;
+}.
+
+Record semantics liA liB := {
+  skel: AST.program unit unit;
+  state: Type;
+  activate :> Genv.symtbl -> lts liA liB state;
+}.
+
+Notation " 'Step' L " := (step L (globalenv L)) (at level 1) : smallstep_scope.
+Notation " 'Star' L " := (star (step L) (globalenv L)) (at level 1) : smallstep_scope.
+Notation " 'Plus' L " := (plus (step L) (globalenv L)) (at level 1) : smallstep_scope.
+
+Section FSIM.
+
+  Context {liA1 liA2} (ccA: callconv liA1 liA2).
+  Context {liB1 liB2} (ccB: callconv liB1 liB2).
+  Context (se1 se2: Genv.symtbl) (wB: ccworld ccB).
+  Context {state1 state2: Type}.
+
+  (** The general form of a forward simulation. *)
+
+  Record fsim_properties (L1: lts liA1 liB1 state1) (L2: lts liA2 liB2 state2) (index: Type)
+         (order: index -> index -> Prop)
+         (match_states: index -> state1 -> state2 -> Prop) : Prop := {
+    fsim_match_valid_query:
+      forall q1 q2, match_query ccB wB q1 q2 ->
+      valid_query L2 q2 = valid_query L1 q1;
+    fsim_match_initial_states:
+      forall q1 q2 s1, match_query ccB wB q1 q2 -> initial_state L1 q1 s1 ->
+      exists i, exists s2, initial_state L2 q2 s2 /\ match_states i s1 s2;
+    fsim_match_final_states:
+      forall i s1 s2 r1, match_states i s1 s2 -> final_state L1 s1 r1 ->
+      exists r2, final_state L2 s2 r2 /\ match_reply ccB wB r1 r2;
+    fsim_match_external:
+      forall i s1 s2 q1, match_states i s1 s2 -> at_external L1 s1 q1 ->
+      exists w q2, at_external L2 s2 q2 /\ match_query ccA w q1 q2 /\ match_senv ccA w se1 se2 /\
+      forall r1 r2 s1', match_reply ccA w r1 r2 -> after_external L1 s1 r1 s1' ->
+      exists i' s2', after_external L2 s2 r2 s2' /\ match_states i' s1' s2';
+    fsim_simulation:
+      forall s1 t s1', Step L1 s1 t s1' ->
+      forall i s2, match_states i s1 s2 ->
+      exists i', exists s2',
+          (Plus L2 s2 t s2' \/ (Star L2 s2 t s2' /\ order i' i))
+      /\ match_states i' s1' s2';
+  }.
+
+Arguments fsim_properties : clear implicits.
+
+Section FORWARD_SIMU_DIAGRAMS.
+
+Variable L1: lts liA1 liB1 state1.
+Variable L2: lts liA2 liB2 state2.
+
+Variable match_states: state1 -> state2 -> Prop.
+
+Hypothesis match_valid_query:
+  forall q1 q2, match_query ccB wB q1 q2 ->
+  valid_query L2 q2 = valid_query L1 q1.
+
+Hypothesis match_initial_states:
+  forall q1 q2 s1, match_query ccB wB q1 q2 -> initial_state L1 q1 s1 ->
+  exists s2, initial_state L2 q2 s2 /\ match_states s1 s2.
+
+Hypothesis match_final_states:
+  forall s1 s2 r1, match_states s1 s2 -> final_state L1 s1 r1 ->
+  exists r2, final_state L2 s2 r2 /\ match_reply ccB wB r1 r2.
+
+Hypothesis match_external:
+  forall s1 s2 q1, match_states s1 s2 -> at_external L1 s1 q1 ->
+  exists wA q2, at_external L2 s2 q2 /\ match_query ccA wA q1 q2 /\ match_senv ccA wA se1 se2 /\
+  forall r1 r2 s1', match_reply ccA wA r1 r2 -> after_external L1 s1 r1 s1' ->
+  exists s2', after_external L2 s2 r2 s2' /\ match_states s1' s2'.
+
+Let ms idx s1 s2 := idx = s1 /\ match_states s1 s2.
+
+Section SIMULATION_STAR_WF.
+
+(** [order] is a well-founded ordering associated with states
+  of the first semantics.  Stuttering steps must correspond
+  to states that decrease w.r.t. [order]. *)
+
+Variable order: state1 -> state1 -> Prop.
+
+Hypothesis simulation:
+  forall s1 t s1', Step L1 s1 t s1' ->
+  forall s2, match_states s1 s2 ->
+  exists s2',
+  (Plus L2 s2 t s2' \/ (Star L2 s2 t s2' /\ order s1' s1))
+  /\ match_states s1' s2'.
+
+Lemma forward_simulation_star_wf:
+  fsim_properties L1 L2 state1 order ms.
+Proof.
+  subst ms;
+  constructor.
+- auto.
+- intros. exploit match_initial_states; eauto. intros [s2 [A B]].
+    exists s1; exists s2; auto.
+- intros. destruct H. eapply match_final_states; eauto.
+- intros. destruct H. edestruct match_external as (w & q2 & H2 & Hq & Hw & Hr); eauto.
+  exists w, q2. intuition auto. edestruct Hr as (s2' & Hs2' & Hs'); eauto.
+- intros. destruct H0. subst i. exploit simulation; eauto. intros [s2' [A B]].
+  exists s1'; exists s2'; intuition auto.
+Qed.
+
+End SIMULATION_STAR_WF.
+
+Section SIMULATION_STAR.
+
+(** We now consider the case where we have a nonnegative integer measure
+  associated with states of the first semantics.  It must decrease when we take
+  a stuttering step. *)
+
+Variable measure: state1 -> nat.
+
+Hypothesis simulation:
+  forall s1 t s1', Step L1 s1 t s1' ->
+  forall s2, match_states s1 s2 ->
+  (exists s2', Plus L2 s2 t s2' /\ match_states s1' s2')
+  \/ (measure s1' < measure s1 /\ t = E0 /\ match_states s1' s2)%nat.
+
+Lemma forward_simulation_star:
+  fsim_properties L1 L2 state1 (ltof _ measure) ms.
+Proof.
+  apply forward_simulation_star_wf.
+  intros. exploit simulation; eauto. intros [[s2' [A B]] | [A [B C]]].
+  exists s2'; auto.
+  exists s2; split. right; split. rewrite B. apply star_refl. auto. auto.
+Qed.
+
+End SIMULATION_STAR.
+
+Section SIMULATION_PLUS.
+
+Hypothesis simulation:
+  forall s1 t s1', Step L1 s1 t s1' ->
+  forall s2, match_states s1 s2 ->
+  exists s2', Plus L2 s2 t s2' /\ match_states s1' s2'.
+
+Lemma forward_simulation_plus:
+  fsim_properties L1 L2 state1 (ltof _ (fun _ => O)) ms.
+Proof.
+  apply forward_simulation_star.
+  intros. exploit simulation; eauto.
+Qed.
+
+End SIMULATION_PLUS.
+
+Section SIMULATION_STEP.
+
+Hypothesis simulation:
+  forall s1 t s1', Step L1 s1 t s1' ->
+  forall s2, match_states s1 s2 ->
+  exists s2', Step L2 s2 t s2' /\ match_states s1' s2'.
+
+Lemma forward_simulation_step:
+  fsim_properties L1 L2 state1 (ltof _ (fun _ => O)) ms.
+Proof.
+  apply forward_simulation_plus.
+  intros. exploit simulation; eauto. intros [s2' [A B]].
+  exists s2'; split; auto. apply plus_one; auto.
+Qed.
+
+End SIMULATION_STEP.
+
+End FORWARD_SIMU_DIAGRAMS.
+
+End FSIM.
+
+Arguments fsim_properties {_ _} _ {_ _} _ _ _ _ {_ _} L1 L2 index order match_states.
+
+Record fsim_components {liA1 liA2} (ccA: callconv liA1 liA2) {liB1 liB2} ccB L1 L2 :=
+  Forward_simulation {
+    fsim_index: Type;
+    fsim_order: fsim_index -> fsim_index -> Prop;
+    fsim_match_states: _;
+
+    fsim_skel:
+      skel L1 = skel L2;
+    fsim_lts se1 se2 wB:
+      @match_senv liB1 liB2 ccB wB se1 se2 ->
+      Genv.valid_for (skel L1) se1 ->
+      fsim_properties ccA ccB se1 se2 wB (activate L1 se1) (activate L2 se2)
+        fsim_index fsim_order (fsim_match_states se1 se2 wB);
+    fsim_order_wf:
+      well_founded fsim_order;
+  }.
+
+Arguments Forward_simulation {_ _ ccA _ _ ccB L1 L2 fsim_index}.
+
+Definition forward_simulation {liA1 liA2} ccA {liB1 liB2} ccB L1 L2 :=
+  inhabited (@fsim_components liA1 liA2 ccA liB1 liB2 ccB L1 L2).
