297c5834d0980f9d1e47945adcc9184d84eea742
Hua-hana <794358120@qq.com>
2022-11-16 09:44:03 +0800

    add generation of relocation entry from instruction

--- riscV/ReloctablesgenArchi.v
+++ riscV/ReloctablesgenArchi.v
@@ -3,29 +3,59 @@ Require Import Asm.
 Require Import Errors.
 Require Import Memtype.
 Require Import RelocProg RelocProgram.
+Require Import RelocationTypes.
 Import ListNotations.
 
 Set Implicit Arguments.
 
 Local Open Scope error_monad_scope.
 
-(** ** Translation of instructions *)
+(** ** Generation of relocation entries *)
 
-Definition instr_reloc_offset (i: instruction) : option Z :=
+Definition transl_instr (sofs:Z) (i: instruction) : res (option relocentry) :=
   match i with
-  | Plb    _ _ (Ofslow _ _) 
-  | Plbu   _ _ (Ofslow _ _) 
-  | Plh    _ _ (Ofslow _ _) 
-  | Plhu   _ _ (Ofslow _ _) 
-  | Plw    _ _ (Ofslow _ _) 
-  | Plw_a  _ _ (Ofslow _ _) 
-  | Pld    _ _ (Ofslow _ _) 
-  | Pld_a  _ _ (Ofslow _ _) 
-  | Psb    _ _ (Ofslow _ _) 
-  | Psh    _ _ (Ofslow _ _) 
-  | Psw    _ _ (Ofslow _ _) 
-  | Psw_a  _ _ (Ofslow _ _) 
-  | Psd    _ _ (Ofslow _ _) 
-  | Psd_a  _ _ (Ofslow _ _) =>
+  (* instructions with raw symbol *)
+  | Plui_s _ symb ofs =>
+      OK (Some {|reloc_offset := sofs; 
+                 reloc_type := R_RISCV_HI20;
+                 reloc_symb := symb;
+                 reloc_addend := ofs |})
+  | Paddi_s _ _ symb ofs =>
+      OK (Some {|reloc_offset := sofs; 
+                 reloc_type := R_RISCV_LO12_I;
+                 reloc_symb := symb;
+                 reloc_addend := ofs |})
+  | Pjal_ofs _ (inl symb) =>
+      OK (Some {|reloc_offset := sofs; 
+                 reloc_type := R_RISCV_JAL;
+                 reloc_symb := symb;
+                 reloc_addend := 0 |})
+         
+  (* load instructions: I type *)
+  | Plb  _ _ (Ofslow symb ofs)
+  | Plbu _ _ (Ofslow symb ofs)
+  | Plh  _ _ (Ofslow symb ofs)
+  | Plhu _ _ (Ofslow symb ofs)        
+  | Plw  _ _ (Ofslow symb ofs)
+  | Pld  _ _ (Ofslow symb ofs)
+  | Pfls _ _ (Ofslow symb ofs)
+  | Pfld _ _ (Ofslow symb ofs) =>
+      OK (Some {|reloc_offset := sofs; 
+                 reloc_type := R_RISCV_LO12_I;
+                 reloc_symb := symb;
+                 reloc_addend := (Ptrofs.signed ofs) |})
+               
+  (* store instructions: S type *)
+  | Psb  _ _ (Ofslow symb ofs)
+  | Psh  _ _ (Ofslow symb ofs)
+  | Psw  _ _ (Ofslow symb ofs)
+  | Psd  _ _ (Ofslow symb ofs)
+  | Pfss _ _ (Ofslow symb ofs)
+  | Pfsd _ _ (Ofslow symb ofs) =>
+      OK (Some {|reloc_offset := sofs; 
+                 reloc_type := R_RISCV_LO12_S;
+                 reloc_symb := symb;
+                 reloc_addend := (Ptrofs.signed ofs)|})
+  | _ => OK None
+  end.
       
-
