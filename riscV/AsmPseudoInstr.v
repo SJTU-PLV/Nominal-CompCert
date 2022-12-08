c9984654ae56d654723b8ed81622aa9a76446e86
Hua-hana <794358120@qq.com>
2022-12-08 10:59:05 +0800

    Revert "low 12 bit and high 20 bit generation for some instructions"
    This reverts commit 34c6a57f13202b048f7c5efb24b209c865baa522.

--- riscV/AsmPseudoInstr.v
+++ riscV/AsmPseudoInstr.v
@@ -9,27 +9,15 @@ Import ListNotations.
 
 Local Open Scope error_monad_scope.
 
-Definition transf_offset (ofs:offset) : offset :=
-  match ofs with
-  | Ofslow id ofs =>
-      let lo := Ptrofs.sign_ext 12 ofs in
-      Ofslow id lo
-  | _ => ofs
-  end.
-
 Definition transf_instr i : (list instruction) :=
   match i with
   | Ploadsymbol_high rd id ofs =>
-      let lo := Ptrofs.sign_ext 12 ofs in
-      let hi := Ptrofs.shru (Ptrofs.sub ofs lo) (Ptrofs.repr 12) in
-      [Plui_s rd id (Ptrofs.unsigned hi)]
+      [Plui_s rd id (Ptrofs.signed ofs)]
   | Ploadsymbol rd id ofs =>
       (* not consider :  Archi.pic_code *)
       (* I think it is necessary to check the 20-bit bounds for ofs when generating reocation entry for Plui_s *)
       (* We expand Ploadsymbol here because the id_eliminate in assembler is one to one *)
-      let lo := Ptrofs.sign_ext 12 ofs in
-      let hi := Ptrofs.shru (Ptrofs.sub ofs lo) (Ptrofs.repr 12) in
-      [Plui_s rd id (Ptrofs.unsigned hi); Paddi_s rd rd id (Ptrofs.signed lo)]
+      [Plui_s rd id (Ptrofs.signed ofs); Paddi_s rd rd id (Ptrofs.signed ofs)]
         
   (* transform pseudo jump instruction *)
   | Pj_s id sg => [Pauipc X31 (inl id); Pjal_rr X0 X31 0]
@@ -39,47 +27,18 @@ Definition transf_instr i : (list instruction) :=
 
   (* we remove the pseudo instruction that use any type here *)
   | Plw_a rd ra ofs =>
-    [Plw rd ra (transf_offset ofs)]
+    [Plw rd ra ofs]
   | Pld_a rd ra ofs =>
-    [Pld rd ra (transf_offset ofs)]
+    [Pld rd ra ofs]
   | Psw_a rs ra ofs =>
-    [Psw rs ra (transf_offset ofs)]
+    [Psw rs ra ofs]
   | Psd_a rs ra ofs =>
-    [Psd rs ra (transf_offset ofs)]
+    [Psd rs ra ofs]
   | Pfld_a rd ra ofs =>
-    [Pfld rd ra (transf_offset ofs)]
+    [Pfld rd ra ofs]
   | Pfsd_a rs ra ofs =>
-    [Pfsd rs ra (transf_offset ofs)]
+    [Pfsd rs ra ofs]
 
-  (* lower 12 bit offset *)
-  | Plb  rd rs ofs => [Plb  rd rs (transf_offset ofs)]
-  | Plbu rd rs ofs => [Plbu rd rs (transf_offset ofs)]
-  | Plh  rd rs ofs => [Plh  rd rs (transf_offset ofs)]
-  | Plhu rd rs ofs => [Plhu rd rs (transf_offset ofs)]
-  | Plw  rd rs ofs => [Plw  rd rs (transf_offset ofs)]
-  | Pld  rd rs ofs => [Pld  rd rs (transf_offset ofs)]
-  | Pfls rd rs ofs => [Pfls rd rs (transf_offset ofs)]
-  | Pfld rd rs ofs => [Pfld rd rs (transf_offset ofs)]
-  | Psb  rd rs ofs => [Psb  rd rs (transf_offset ofs)]
-  | Psh  rd rs ofs => [Psh  rd rs (transf_offset ofs)]
-  | Psw  rd rs ofs => [Psw  rd rs (transf_offset ofs)]
-  | Psd  rd rs ofs => [Psd  rd rs (transf_offset ofs)]
-  | Pfss rd rs ofs => [Pfss rd rs (transf_offset ofs)]
-  | Pfsd rd rs ofs => [Pfsd rd rs (transf_offset ofs)]
-
-  (* high 20 bit *)
-  | Pluil rd ofs => 
-      let lo := Int64.sign_ext 12 ofs in
-      let hi := Int64.shru (Int64.sub ofs lo) (Int64.repr 12) in
-      [Pluil rd hi]
-  | Pluiw rd ofs =>
-      let lo := Int.sign_ext 12 ofs in
-      let hi := Int.shru (Int.sub ofs lo) (Int.repr 12) in
-      if Archi.ptr64 then
-        [Pluil rd (Int64.repr (Int.unsigned hi))]
-      else
-        [Pluiw rd hi]        
-      
   (* Some pseudo instructions in riscv manual *)
   | Pmv rd rs =>
     if Archi.ptr64 then
@@ -108,10 +67,10 @@ Definition transf_instr i : (list instruction) :=
       if Archi.ptr64 then
         [Pxoril rd rs (Int64.repr (Int.signed imm))]
       else [i]
-  (* | Pluiw rd imm => *)
-  (*     if Archi.ptr64 then *)
-  (*       [Pluil rd (Int64.repr (Int.signed imm))] *)
-  (*     else [i] *)
+  | Pluiw rd imm =>
+      if Archi.ptr64 then
+        [Pluil rd (Int64.repr (Int.signed imm))]
+      else [i]
   | Psltw rd rs imm =>
       if Archi.ptr64 then
         [Psltl rd rs imm]
