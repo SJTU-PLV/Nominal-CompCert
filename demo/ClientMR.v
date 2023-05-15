32fe74026168f1ef7a9a4e5f2795b2d84338bebe
Ling Zhang <ling.zhang@sjtu.edu.cn>
2023-05-15 20:47:12 +0800

    Defined new client for MutRec Example

--- demo/ClientMR.v
+++ demo/ClientMR.v
@@ -31,46 +31,34 @@ Definition result_id := 4%positive.
 Definition request_id := 6%positive.
 *)
 
+Definition N := 10%positive.
 
-Definition result_def :=  {|
+Definition resultN_def :=  {|
+  gvar_info := Tarray tint 10 noattr;
+  gvar_init := nil;
+  gvar_readonly := false;
+  gvar_volatile := false
+|}.
+
+Definition input_id := 10%positive.
+Definition inputN_def :=  {|
+  gvar_info := tarray tint 10;
+  gvar_init := nil;
+  gvar_readonly := false;
+  gvar_volatile := false
+|}.
+
+Definition index_id := 11%positive.
+Definition index_def :=  {|
   gvar_info := tint;
   gvar_init := nil;
   gvar_readonly := false;
   gvar_volatile := false
 |}.
 
-(*
-Definition r_id := 7%positive.
- *)
-
-Definition func_process :=
-  {|
-    fn_return := Tvoid;
-    fn_callconv := cc_default;
-    fn_params := (r_id,tint) :: nil;
-    fn_vars := nil;
-    fn_temps := nil;
-    fn_body :=
-      (Ssequence
-         (* result = r; *)
-         (Sassign (Evar result_id tint) (Evar r_id tint))
-         (* return; *)
-         (Sreturn None)
-      )
-  |}.
-
-Definition i_id := 8%positive.
-Definition func_request :=
-  {|
-    fn_return := Tvoid;
-    fn_callconv := cc_default;
-    fn_params := (i_id, tint) :: nil;
-    fn_vars := nil;
-    fn_temps := nil;
-    fn_body :=
-      (Ssequence
-         (* encrypt (i,process) *)
-         (Scall None
+(* The call instruction of encrypt with argument input *)
+Definition call_encrypt' input :=
+  (Scall None
             (*function name and signature*)
             (Evar encrypt_id
                (Tfunction
@@ -81,14 +69,53 @@ Definition func_request :=
                   Tvoid cc_default)
             )
             (*arguments*)
-            ((Evar i_id tint) :: (Evar process_id (Tfunction (Tcons tint Tnil) Tvoid cc_default)) :: nil)
+            (input :: (Evar process_id (Tfunction (Tcons tint Tnil) Tvoid cc_default)) :: nil)
+  ).
+
+(* the expr input[index] *)
+Definition input_index :=
+  Ederef (Ebinop Oadd (Evar input_id (tarray tint 10)) (Evar index_id tint) (tptr tint)) tint.
+
+Definition result_index :=
+  Ederef (Ebinop Oadd (Evar result_id (tarray tint 10)) (Evar index_id tint) (tptr tint)) tint.
+
+(* encrypt (input[index++], request); *)
+Definition call_encrypt_indexplus :=
+  Ssequence
+    (* index = index + 1*)
+    (Sassign (Evar index_id tint) (Ebinop Oadd (Evar index_id tint) (Econst_int (Int.repr 1) tint) tint))
+    (call_encrypt' input_index).
+
+Definition if_index_eq_0 :=
+  Ebinop Oeq (Evar index_id tint) (Econst_int (Int.repr 0) tint) tbool.
+
+Definition if_index_gt_0_le_N :=
+  Ebinop Oand
+    (Ebinop Ogt (Evar index_id tint) (Econst_int (Int.repr 0) tint) tbool)
+    (Ebinop Ole (Evar index_id tint) (Econst_int (Int.repr 10) tint) tbool)
+    tbool.
+
+Definition func_request :=
+  {|
+    fn_return := Tvoid;
+    fn_callconv := cc_default;
+    fn_params := (r_id,tint) :: nil;
+    fn_vars := nil;
+    fn_temps := nil;
+    fn_body :=
+      (Sifthenelse if_index_eq_0 (** index == 0*)
+         call_encrypt_indexplus (** encrypt (input[index++], request) *)
+         (Sifthenelse if_index_gt_0_le_N (** 0 < index <= N*)
+            (Ssequence
+               (Sassign (result_index) (Etempvar r_id tint)) (** result[index] = r*)
+               call_encrypt_indexplus (** encrypt (input[index++], request) *)
+            )
+            (Sreturn None) (** return; *)
          )
-         (Sreturn None)
       )
   |}.
 
 Definition composites : list composite_definition := nil.
-
 
 Definition func_encrypt_external : fundef :=
   (External (EF_external "encrypt" int_fptr__void_sg)
@@ -98,14 +125,15 @@ Definition func_encrypt_external : fundef :=
 
 Definition global_definitions_client : list (ident * globdef fundef type) :=
 (encrypt_id,
-  Gfun func_encrypt_external) ::
- (request_id, Gfun(Internal func_request)) ::
- (process_id, Gfun(Internal func_process)) ::
- (result_id, Gvar result_def) ::
+ Gfun func_encrypt_external) ::
+  (request_id, Gfun(Internal func_request)) ::
+ (input_id, Gvar inputN_def) ::
+ (result_id, Gvar resultN_def) ::
+ (index_id, Gvar index_def) ::
  nil.
 
 Definition public_idents_client : list ident :=
-(encrypt_id :: request_id :: process_id :: result_id :: nil).
+(encrypt_id :: request_id :: input_id :: result_id :: index_id :: nil).
 
 Definition client : Clight.program :=
   mkprogram composites global_definitions_client public_idents_client main_id Logic.I.
