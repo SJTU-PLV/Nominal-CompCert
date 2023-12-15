debe72c7188cedaa554519769889b1ae348624c8
Jinhua Wu <jinhua.wu@sjtu.edu.cn>
2023-12-15 22:43:50 +0800

    updated RustIRgen, only implement CFG generation, leave drop elaboration as the next pass

--- rustfrontend/RustIRgen.v
+++ rustfrontend/RustIRgen.v
@@ -18,7 +18,6 @@ operation based on the ownership semantics in the source program. *)
 (** * Translation state *)
 
 Record state: Type := mkstate {
-  st_nexttemps: ident;
   st_nextblock: bb;
   st_code: cfg;
   st_stmts: list statement;
@@ -31,7 +30,6 @@ Inductive state_incr: state -> state -> Prop :=
   state_incr_intro:
     forall (s1 s2: state),
     Ple s1.(st_nextblock) s2.(st_nextblock) ->
-    Ple s1.(st_nexttemps) s2.(st_nexttemps) ->
     (* (forall bid, *)
     (*     (** FIXME: exists stmts, s2.stmt = s1.stmt + stmts *) *)
     (*   s1.(st_code)!bid = None \/ s2.(st_code)!bid = s1.(st_code)!bid) -> *)
@@ -42,7 +40,7 @@ Lemma state_incr_refl:
   forall s, state_incr s s.
 Proof.
   intros. apply state_incr_intro.
-  apply Ple_refl. apply Ple_refl. (* intros; auto. *)
+  apply Ple_refl. 
 Qed.
 
 Lemma state_incr_trans:
@@ -50,7 +48,6 @@ Lemma state_incr_trans:
 Proof.
   intros. inv H; inv H0. apply state_incr_intro.
   apply Ple_trans with (st_nextblock s2); assumption.
-  apply Ple_trans with (st_nexttemps s2); assumption.
   (* intros. generalize (H3 bid) (H5 bid). intuition congruence. *)
 Qed.
 
@@ -121,8 +118,8 @@ Definition move_path_index (me: move_path_env) (p: place) : option ident :=
 
 (** The initial state (empty CFG). *)
 
-Definition init_state (idents: list ident) (t: terminator) : state :=
-  mkstate (fold_right Pos.max 1%positive idents) 1%positive (PTree.empty basic_block) nil t.
+Definition init_state (t: terminator) : state :=
+  mkstate 1%positive (PTree.empty basic_block) nil t.
 
 
 (** Add IR statement in the current block *)
@@ -130,7 +127,6 @@ Definition init_state (idents: list ident) (t: terminator) : state :=
 Remark add_instr_incr:
   forall s stmt,
     state_incr s (mkstate
-                    s.(st_nexttemps)
                     s.(st_nextblock)
                     s.(st_code)
                     (stmt :: s.(st_stmts))
@@ -138,31 +134,30 @@ Remark add_instr_incr:
 Proof.
   constructor; simpl.
   apply Ple_refl.
-  apply Ple_refl.
   (* intros. destruct (st_wf s pc). right. apply PTree.gso. apply Plt_ne; auto. auto. *)
 Qed.
 
 
 Definition add_instr (stmt: statement) : mon unit :=
   fun (s: state) =>
-    OK tt (mkstate s.(st_nexttemps) s.(st_nextblock) s.(st_code) (stmt :: s.(st_stmts)) s.(st_term)) (add_instr_incr s stmt).
+    OK tt (mkstate s.(st_nextblock) s.(st_code) (stmt :: s.(st_stmts)) s.(st_term)) (add_instr_incr s stmt).
 
-Definition add_instr_endlet (local_end: option ident) : mon unit :=
-  match local_end with
-  | Some id =>
-      do _ <- add_instr (Sstoragedead id);
-      (** TODO: insert drop *)
-      ret tt
-  | _ => ret tt
-  end.
+(* add drop statement *)
+Definition add_drop (p: place) : mon unit :=
+  do _ <- add_instr (Sdrop p);
+  ret tt.
 
+
+Definition add_instr_endlet (local: ident) (ty: type) : mon unit :=
+  do _ <- add_drop (Plocal local ty);
+  do _ <- add_instr (Sstoragedead local);
+  ret tt.
 
 (** Generate a new basic block  *)
 
 Remark add_block_incr:
   forall s c stmts term,
-    state_incr s (mkstate
-                    s.(st_nexttemps)
+    state_incr s (mkstate                  
                     (Pos.succ s.(st_nextblock))
                     c
                     stmts
@@ -170,23 +165,88 @@ Remark add_block_incr:
 Proof.
   constructor; simpl.
   apply Ple_succ.
-  apply Ple_refl.
   (* intros. destruct (st_wf s pc). right. apply PTree.gso. apply Plt_ne; auto. auto. *)
 Qed.
 
 (* very strange function: it build a basic block from the current
 state, then use the new_term as the terminator for the next basic
 block to be built, and return the latest block id *)
-Definition add_block (new_term: terminator) : mon bb :=
+Definition add_block (cur: bb) (new_term: terminator) : mon bb :=
   fun (s: state) =>
-    let n := s.(st_nextblock) in
-    let nb := Build_basic_block s.(st_stmts) s.(st_term) in
-    let c := PTree.set n nb s.(st_code) in
-    OK n (mkstate s.(st_nexttemps) (Pos.succ s.(st_nextblock)) c nil new_term) (add_block_incr s c nil new_term).
+    match plt cur s.(st_nextblock) with
+    | left LT =>        
+        let nb := Build_basic_block s.(st_stmts) s.(st_term) in
+        let c := PTree.set cur nb s.(st_code) in
+        OK s.(st_nextblock) (mkstate (Pos.succ s.(st_nextblock)) c nil new_term) (add_block_incr s c nil new_term)
+    | _ =>
+        Error (Errors.msg "RustIRgen.add_block")
+    end.
 
+Remark reserve_block_incr:
+  forall s,
+    state_incr s (mkstate (Pos.succ s.(st_nextblock)) s.(st_code) s.(st_stmts) s.(st_term)).
+Proof.
+  constructor; simpl.
+  apply Ple_succ.
+Qed.
+
+Definition reserve_block : mon bb :=
+  fun (s: state) =>
+    OK s.(st_nextblock) (mkstate (Pos.succ s.(st_nextblock)) s.(st_code) s.(st_stmts) s.(st_term)) (reserve_block_incr s).
+
+
+Remark construct_block_incr:
+  forall s c stmts,
+    state_incr s (mkstate                  
+                    s.(st_nextblock)
+                    c
+                    stmts
+                    s.(st_term)).
+Proof.
+  constructor; simpl.
+  apply Ple_refl.
+  (* intros. destruct (st_wf s pc). right. apply PTree.gso. apply Plt_ne; auto. auto. *)
+Qed.
+
+Definition construct_block (cur: bb) : mon unit :=
+  fun (s: state) =>
+    match plt cur s.(st_nextblock) with
+    | left LT =>        
+        let nb := Build_basic_block s.(st_stmts) s.(st_term) in
+        let c := PTree.set cur nb s.(st_code) in
+        OK tt (mkstate s.(st_nextblock) c nil s.(st_term)) (construct_block_incr s c nil)
+    | _ =>
+        Error (Errors.msg "RustIRgen.construct_block")
+    end.
+
+Remark update_terminator_incr:
+  forall s t,
+    state_incr s (mkstate                  
+                    s.(st_nextblock)
+                    s.(st_code)
+                    s.(st_stmts)
+                    t).
+Proof.
+  constructor; simpl.
+  apply Ple_refl.
+Qed.
+
+Definition update_terminator (t: terminator) : mon unit :=
+  fun (s: state) =>
+    OK tt (mkstate s.(st_nextblock) s.(st_code) s.(st_stmts) t) (update_terminator_incr s t).
+
+Definition list_list_cons {A: Type} (e: A) (l: list (list A)) :=
+  match l with
+  | nil => (e::nil)::nil
+  | l' :: l => (e::l') :: l
+  end.
+
+
+Definition add_drop_list (locals: list (ident * type)) : mon unit :=
+  fold_right (fun elt acc => do _ <- acc; add_instr_endlet (fst elt) (snd elt)) (ret tt) locals.
 
 (** Translation of expression  *)
-
+(* unused: drop flag is generated in next pass *)
 Fixpoint transl_expr (me: move_path_env) (e: expr) : mon (list statement) :=
   match e with
   | Eplace Move p ty =>
@@ -207,46 +267,123 @@ Fixpoint transl_expr (me: move_path_env) (e: expr) : mon (list statement) :=
 
 (** Translation of statement *)
 
-Fixpoint transl_stmt (me: move_path_env) (stmt: RustlightBase.statement) (succ: bb) (local_end: option ident) {struct stmt} : mon bb :=
-  fun (s: state) =>
-    do _ <- add_instr_endlet local_end;
+Fixpoint transl_stmt (stmt: RustlightBase.statement) (succ: bb) (live_vars: list (list (ident * type))) (cont: option bb) (break: option bb) {struct stmt} : mon bb :=
     match stmt with
     | Sskip =>
         ret succ
     | Slet id ty e stmt' =>
-        do succ' <- transl_stmt me stmt' succ (Some id);
+        do _ <- add_instr_endlet id ty;
+        do succ' <- transl_stmt stmt' succ (list_list_cons (id,ty) live_vars) cont break;
         do _ <- add_instr (Sassign (Plocal id ty) e);
         do _ <- add_instr (Sstoragelive id);
         ret succ'
+    | RustlightBase.Sassign p e =>
+        do _ <- add_drop p;
+        do _ <- add_instr (Sassign p e);
+        ret succ
     | Ssequence stmt1 stmt2 =>
-        do succ2 <- transl_stmt me stmt2 succ None;
-        do succ1 <- transl_stmt me stmt1 succ2 None;
+        do succ2 <- transl_stmt stmt2 succ live_vars cont break;
+        do succ1 <- transl_stmt stmt1 succ2 live_vars cont break;
         ret succ1
     | Sifthenelse e stmt1 stmt2 =>
-        do prev_succ1 <- add_block (Tgoto succ);
-        do bb1 <- transl_stmt me stmt1 prev_succ1 None;
-        do prev_succ2 <- add_block (Tgoto succ);
-        do bb2 <- transl_stmt me stmt2 prev_succ2 None;
-        do retb <- add_block (Tifthenelse e bb1 bb2);
+        do prev_succ1 <- add_block succ (Tgoto succ);
+        do bb1 <- transl_stmt stmt1 prev_succ1 live_vars cont break;
+        do prev_succ2 <- add_block bb1 (Tgoto succ);
+        do bb2 <- transl_stmt stmt2 prev_succ2 live_vars cont break;
+        do retb <- add_block bb2 (Tifthenelse e bb1 bb2);
         ret retb
-    | Sloop stmt1 stmt2 =>
-        (** Very Hard !  *)
-            
+    | Sloop stmt =>
+        do loop_start <- reserve_block;
+        do body_end <- add_block succ (Tgoto loop_start);
+        do body_start <- transl_stmt stmt body_end (nil::live_vars) (Some loop_start) (Some succ);
+        do _ <- construct_block body_start;
+        do _ <- update_terminator (Tgoto body_start);
+        ret loop_start          (* to construct the block for loop_start *)
+    | Sbreak =>
+        match break with
+        | None =>
+            error (Errors.msg "No loop outside the break")
+        | Some brk =>            
+            match live_vars with
+            | nil =>             (* nothing to drop *)
+                do prev <- add_block succ (Tgoto brk);
+                ret prev
+            | locals :: _ =>
+                do drop_block <- add_block succ (Tgoto brk);
+                (* insert drops in drop_block *)                              
+                do _ <- add_drop_list locals;
+                do prev <- add_block drop_block (Tgoto drop_block);
+                ret prev
+            end
+        end
+    | Scontinue =>
+        match cont with
+        | None =>
+            error (Errors.msg "No loop outside the continue")
+        | Some cont =>            
+            match live_vars with
+            | nil =>
+                do prev <- add_block succ (Tgoto cont);
+                ret prev
+            | locals :: _ =>
+                do drop_block <- add_block succ (Tgoto cont);
+                (* insert drops in drop_block *)                              
+                do _ <- add_drop_list locals;
+                do prev <- add_block drop_block (Tgoto drop_block);
+                ret prev
+            end
+        end
+    | Scall p a al =>
+        do call_block <- add_block succ (Tcall p a al succ);
+        ret call_block
+    | Sreturn e =>
+        do return_block <- add_block succ (Treturn e);      
+        let locals := concat live_vars in        
+        do _ <- add_drop_list locals;
+        do prev <- add_block return_block (Tgoto return_block);
+        ret prev
+    end.
+
+
+Fixpoint extract_vars (stmt: RustlightBase.statement) : list (ident * type) :=
+  match stmt with
+  | Slet id ty _ s =>
+      (id,ty) :: extract_vars s
+  | Ssequence s1 s2 =>
+      extract_vars s1 ++ extract_vars s2
+  | Sifthenelse _ s1 s2 =>
+      extract_vars s1 ++ extract_vars s2
+  | Sloop s =>
+      extract_vars s
+  | _ => nil
+  end.
+
 (** Translation of function  *)
 
 (* return the entry block and the temporary variables *)
-Definition transl_fun (f: RustlightBase.function) : mon (bb * list (ident * type)) :=
-  
-
+Definition transl_fun (f: RustlightBase.function) : mon bb :=
+  do return_block <- reserve_block;
+  (* add drops for the function parameter in return_block *)
+  do _ <- fold_right (fun elt acc => do _ <- acc; add_drop (Plocal (fst elt) (snd elt))) (ret tt) f.(RustlightBase.fn_params);
+  do return_prev <- add_block return_block (Tgoto return_block);
+  do entry <- transl_stmt f.(RustlightBase.fn_body) return_prev (f.(RustlightBase.fn_params) :: nil) None None;
+  do _ <- construct_block entry;
+  ret entry.
 
 
 Definition transl_function (f: RustlightBase.function) : Errors.res function :=
-  do vars <- extract_vars f.(fn_body);
-  let init_state := init_state (map fst (vars ++ f.(fn_params))) in
+  let vars := extract_vars f.(RustlightBase.fn_body) in
+  let init_state := init_state (Treturn None) in
   match transl_fun f init_state with
-
-                        
-
-
-  
+  | Error msg => Errors.Error msg
+  | OK entry s i =>
+      Errors.OK (Build_function
+                   f.(RustlightBase.fn_return)
+                   f.(RustlightBase.fn_callconv)
+                   f.(RustlightBase.fn_params)
+                   vars
+                   nil
+                   s.(st_code)
+                  entry)
+  end.
   
