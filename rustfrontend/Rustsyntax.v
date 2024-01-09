6d7886ef490d559ea8dcfd379621fdba974324b8
Jinhua Wu <jinhua.wu@sjtu.edu.cn>
2024-01-10 01:03:27 +0800

    work in defining notations for rustsyntax

--- rustfrontend/Rustsyntax.v
+++ rustfrontend/Rustsyntax.v
@@ -74,3 +74,56 @@ Record function : Type := mkfunction {
 Definition fundef := Rusttypes.fundef function.
 
 Definition program := Rusttypes.program function.
+
+
+(** Notations for Rustsyntax programs *)
+
+
+Definition A : ident := 1%positive.
+Definition B : ident := 2%positive.
+Definition C : ident := 3%positive.
+Definition D : ident := 4%positive.
+Definition E : ident := 5%positive.
+
+Declare Custom Entry rustsyntax.
+Declare Scope rustsyntax_scope.
+Declare Custom Entry rustsyntax_aux.
+
+Notation "<{ s }>" := s (s custom rustsyntax_aux) : rustsyntax_scope.
+Notation "s" := s (in custom rustsyntax_aux at level 0, s custom rustsyntax) : rustsyntax_scope.
+
+(* Notations for statement *)
+Notation "'if' e 'then' s1 'else' s2 'end'" := (Sifthenelse e s1 s2) (in custom rustsyntax at level 89, right associativity) : rustsyntax_scope.
+Notation "s1 ; s2" := (Ssequence s1 s2) (in custom rustsyntax at level 90, right associativity) : rustsyntax_scope.
+Notation "'do' e" := (Sdo e) (in custom rustsyntax at level 80) : rustsyntax_scope.
+Notation "'skip'" := Sskip (in custom rustsyntax at level 0) : rustsyntax_scope.
+Notation "'break'" := Sbreak (in custom rustsyntax at level 0) : rustsyntax_scope.
+Notation "'continue'" := Scontinue (in custom rustsyntax at level 0) : rustsyntax_scope.
+Notation "'return'" := (Sreturn None) (in custom rustsyntax at level 10) : rustsyntax_scope.
+Notation "'return' e" := (Sreturn (@Some expr e)) (in custom rustsyntax at level 10) : rustsyntax_scope.
+Notation "'let' x : t 'in' s 'end' " := (Slet x t s) (in custom rustsyntax at level 5, x constr at level 99, t constr at level 99) : rustsyntax_scope.
+Notation "'loop' s 'end'" := (Sloop s) (in custom rustsyntax at level 20) : rustsyntax_scope.
+Notation "'while' e 'do' s 'end'" := (Swhile e s) (in custom rustsyntax at level 20) : rustsyntax_scope.
+(** TODO: define the notations for match statement *)
+
+(* Notations for expression *)
+
+Notation " 'Var' x # t " := (Evar x t) (in custom rustsyntax at level 10, x global, t global).
+Notation "'Box' < t > ( e )" := (Ebox e t) (in custom rustsyntax at level 10, t global).
+
+Print Grammar constr.
+Print Custom Grammar rustsyntax.
+
+Open Scope rustsyntax_scope.
+
+Definition var_a : expr := <{ Var A # type_int32s }>.
+Definition box_a : expr := <{ Box< type_int32s >(Var A # type_int32s) }>.
+
+
+Notation "p := f ( l ) " := (Scall (Some p) f l) (in custom rustsyntax at level 5, p constr at level 99, f constr at level 99, l constr at level 99, no associativity) : rustsyntax_scope.
+Notation " f ( l )" := (Scall None f l) (in custom rustsyntax at level 5, f constr at level 99, l constr at level 99, no associativity) : rustsyntax_scope.
+
+
+(* Print Grammar constr. *)
+
+
