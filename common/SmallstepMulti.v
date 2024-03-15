7b2f8105aea8f9e98b9ebd86afa03dbd28f23db1
Ling Zhang <ling.zhang@sjtu.edu.cn>
2024-03-15 19:49:57 +0800

    Defined concur semantics for yield and thread_create, seems we de not need get_mem and set_mem yet

--- common/SmallstepMulti.v
+++ common/SmallstepMulti.v
@@ -54,6 +54,16 @@ Section MultiThread.
 
   Definition local_state := state OpenS.
 
+  Record cq_vals : Type := cqv {cqv_vf : val; cqv_sg : signature; cqv_args : list val}.
+
+  Definition get_query (cqv : cq_vals) (m: mem) :=
+    cq (cqv_vf cqv) (cqv_sg cqv) (cqv_args cqv) m.
+
+  Definition get_cqv (q: c_query) :=
+    cqv (cq_vf q) (cq_sg q) (cq_args q).
+  
+  Definition thread_state :Type := local_state + cq_vals.
+  
   (* Maybe should be somehow added into open semantics *)
   Variable initial_query : query li_c.
   Variable final_reply : int -> reply li_c -> Prop.
@@ -69,8 +79,9 @@ Section MultiThread.
 
   (* We need an interface to get and set global memory from local_state *)
   (* Not sure if this is proper, maybe need some lemmas about these operations *)
-  Variable get_gmem : local_state -> mem.
-  Variable set_gmem : mem -> local_state -> local_state.
+
+  (*Variable get_gmem : local_state -> mem.
+  Variable set_gmem : mem -> local_state -> local_state. *)
 
   (*
   Definition tid_mem (m:mem) : nat :=
@@ -84,7 +95,7 @@ Section MultiThread.
 
     Record state : Type := mk_gstate
     {
-      threads: NatMap.t (option local_state);
+      threads: NatMap.t (option thread_state);
       cur_tid : nat;
       next_tid : nat;
       (* atom_bit : bool; *)
@@ -94,15 +105,15 @@ Section MultiThread.
                          length (stacks_mem (get_gmem ls)) = next_tid; *)
     }.
 
-  Definition empty_threads : NatMap.t (option local_state) := NatMap.init None.
+  Definition empty_threads : NatMap.t (option thread_state) := NatMap.init None.
   
-  Definition update_threads (s: state) (t: NatMap.t (option local_state)) :=
+  Definition update_threads (s: state) (t: NatMap.t (option thread_state)) :=
     mk_gstate t (cur_tid s) (next_tid s) .
 
-  Definition update_thread (s: state) (tid : nat) (ls: local_state) :=
+  Definition update_thread (s: state) (tid : nat) (ls: thread_state) :=
     mk_gstate (NatMap.set tid (Some ls) (threads s)) (cur_tid s) (next_tid s).
 
-  Definition update_cur_thread (s: state) (ls : local_state) := update_thread s (cur_tid s) ls.
+  Definition update_cur_thread (s: state) (ls : thread_state) := update_thread s (cur_tid s) ls.
   
   Definition get_thread (s: state) (tid: nat) :=
     NatMap.get tid (threads s).
@@ -122,23 +133,18 @@ Section MultiThread.
   (* We transfer to thread tid' and update
      its local_state with an updated memory *)
 
-  Definition yield_state (s: state) (gmem: mem): state :=
+  Definition yield_state (s: state) (ls: thread_state): state :=
     let tid' := yield_strategy s in
     let s' := update_cur_tid s tid' in
-    match get_cur_thread s with (*yield_strategy and the inv should guarantee we get Some here*)
-    |Some ls => update_thread s' tid' (set_gmem gmem ls)
-    |None => s'
-    end.
+    update_thread s' tid' ls.
 
-  (* We add a new thread with its initial local state, we also update the running memory by adding a new list of positives *)
-  Definition pthread_create_state (s: state) (ls : local_state) (gmem : mem): state :=
+  (* We add a new thread with its initial query without memory,
+      we also update the running memory by adding a new list of positives *)
+  Definition pthread_create_state (s: state) (cqv : cq_vals) (ls' : thread_state): state :=
     let ntid := (next_tid s) in let ctid := (cur_tid s) in
     let s' := update_next_tid s (S ntid) in
-    let s'' := update_thread s' ntid ls in
-    match get_cur_thread s with
-    |Some ls => update_thread s'' ctid (set_gmem gmem ls)
-    |None => s'
-    end.
+    let s'' := update_thread s' ntid (inr cqv) in
+    update_thread s'' ctid ls'.
     
   Definition genvtype := Smallstep.genvtype OpenLTS.
   
@@ -150,7 +156,7 @@ Section MultiThread.
   Inductive initial_state : state -> Prop :=
   |initial_state_intro : forall ls s,
       cur_tid s = 1%nat -> next_tid s = 2%nat ->
-      get_cur_thread s = Some ls ->
+      get_cur_thread s = Some (inl ls) ->
       (Closed.initial_state ClosedS) ls ->
       initial_state s.
 
@@ -158,7 +164,7 @@ Section MultiThread.
   Inductive final_state : state -> int -> Prop :=  
   |final_state_intro : forall ls i s,
       cur_tid s = 1%nat ->
-      get_cur_thread s = Some ls ->
+      get_cur_thread s = Some (inl ls) ->
       (Closed.final_state ClosedS) ls i->
       final_state s i.
 
@@ -203,26 +209,42 @@ Section MultiThread.
   
   Inductive step : genvtype -> state -> trace -> state -> Prop :=
   |step_local : forall ge ls1 t ls2 s s',
-      get_cur_thread s = Some ls1 ->
+      get_cur_thread s = Some (inl ls1) ->
       Smallstep.step OpenLTS ge ls1 t ls2 ->
-      update_thread s (cur_tid s) ls2 = s' ->
+      update_thread s (cur_tid s) (inl ls2) = s' ->
       step ge s t s'
-  |step_thread_yield : forall ge s s' tid' ls q gmem' p,
-      get_cur_thread s = Some ls ->
+  |step_thread_create : forall ge s s' q_ptc q_str gmem ls ls' cqv,
+      get_cur_thread s = Some (inl ls) -> (* get the current local state *)
+      Smallstep.at_external OpenLTS ls q_ptc -> (* get the query to pthread_create *)
+      query_is_pthread_create q_ptc q_str -> (* get the query to start_routine *)
+      (* The global memory is already updated from q_ptc to q_str *)
+      cq_mem q_str = gmem -> (* the updated memory, only difference is the #threads *)
+      get_cqv q_str = cqv -> (*the initial query without memory, is stored as initial state in new thread *)
+      Smallstep.after_external OpenLTS ls (cr (Vint Int.one) gmem) ls' -> (* the current thread completes the primitive*)
+      pthread_create_state s cqv (inl ls') = s' ->
+      step ge s E0 s'
+  (** yield to a thread which is waiting for the reply of its own [yield()] *)
+  |step_thread_yield_to_yield : forall ge s s' tid' q gmem' p ls ls1 ls1',
+      get_cur_thread s = Some (inl ls) ->
       Smallstep.at_external OpenLTS ls q ->
       query_is_yield q ->
       yield_strategy s = tid' ->
       (*the proof p may be a problem, provided from the invariant between state and the support in gmem *)
-      Mem.yield (cq_mem q) tid' p = gmem' -> 
-      yield_state s gmem' = s' ->
+      Mem.yield (cq_mem q) tid' p = gmem' ->
+      get_thread s (tid') = Some (inl ls1) -> (* the target thread is waiting for reply *)
+      Smallstep.after_external OpenLTS ls1 (cr Vundef gmem') ls1' ->
+      yield_state s (inl ls1') = s' ->
       step ge s E0 s'
-  |step_thread_create : forall ge s s' q_ptc q_str new_ls gmem ls,
-      get_cur_thread s = Some ls ->
-      Smallstep.at_external OpenLTS ls q_ptc ->
-      query_is_pthread_create q_ptc q_str ->
-      Smallstep.initial_state OpenLTS q_str new_ls ->
-      cq_mem q_str = gmem -> (* The global memory is already updated from q_ptc to q_str *)
-      pthread_create_state s new_ls gmem = s' ->
+  (** yield to a thread which has not been initialized from a query *)
+  |step_thread_yield_to_initial : forall ge s s' tid' q gmem' p ls cqv ls1',
+      get_cur_thread s = Some (inl ls) ->
+      Smallstep.at_external OpenLTS ls q ->
+      query_is_yield q ->
+      yield_strategy s = tid' ->
+      Mem.yield (cq_mem q) tid' p = gmem' ->
+      get_thread s (tid') = Some (inr cqv) -> (* the target thread is waiting for reply *)
+      Smallstep.initial_state OpenLTS (get_query cqv gmem') ls1' ->
+      yield_state s (inl ls1') = s' ->
       step ge s E0 s'.
   
 End MultiThread.   
