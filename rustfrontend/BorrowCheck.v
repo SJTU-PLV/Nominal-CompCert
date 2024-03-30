7e81e443783176e494c4a21eb74cfb949c902916
Jinhua Wu <jinhua.wu@sjtu.edu.cn>
2024-03-30 22:32:05 +0800

    init function parameters

--- rustfrontend/BorrowCheck.v
+++ rustfrontend/BorrowCheck.v
@@ -28,10 +28,6 @@ End RUST_TYPE.
 Module TyMap := EMap(RUST_TYPE).
 
 
-Section COMPOSITE_ENV.
-
-Variable ce: composite_env.
-
 (* (* case1: [p] is an owner or a slice of an owner. If [p] stores a location to *)
 (* another owner (memory block) then add [*p] to the result *) *)
 (* (* case2: [p] is a parameter or a slice of a paramerer. If [p] stores a *)
@@ -201,131 +197,152 @@ Variable ce: composite_env.
 (*   do l <- fold_right_bind params (owner_place false); *)
 (*   fold_left allocate_place_acc (concat l) (OK m). *)
 
+Let t := TyMap.t (option ablock) ->
+         type ->
+         ablock ->
+         path ->
+         amem ->
+         positive ->
+         positive ->
+         res (amem * positive * positive * TyMap.t (option ablock) * list (ablock * type)).
 
-(* just initialize the location (b, ph), return the new allocated
-blocks *)
-Fixpoint shallow_initialize (shr_map: TyMap.t (option ablock)) (rec_map: TyMap.t (option aval)) (ty: type) (b: ablock) (ph: path) (m: amem) (next_extern: positive) (next_tag: positive) : res (amem * positive * positive * TyMap.t (option ablock) * TyMap.t (option aval) * (list ablock)) :=
-  (* check whether ty is a recursive type and is associated with an
-  initial abstract value *)    
-  match TyMap.get ty rec_map with
-  | Some v =>
-      (* use v to initialize b *)
-      do m' <- store m b ph v;
+Section SHALLOW_INIT.
+
+Variable ce: composite_env.
+  
+Variable rec: forall (ce': composite_env), (PTree_Properties.cardinal ce' < PTree_Properties.cardinal ce)%nat -> t.
+
+Definition shallow_initialize' (shr_map: TyMap.t (option ablock)) (ty: type) (b: ablock) (ph: path) (m: amem) (next_extern: positive) (next_tag: positive) :  res (amem * positive * positive * TyMap.t (option ablock) * list (ablock * type)) :=
+  match ty with
+  | Tunit
+  | Tint _ _ _
+  | Tlong _ _
+  | Tfloat _ _
+  | Tfunction _ _ _ =>
+      do m' <- store m b ph Scalar;
       OK (m', next_extern, next_tag, shr_map, [])
-  | None =>
-      match ty with
-      | Tunit
-      | Tint _ _ _
-      | Tlong _ _
-      | Tfloat _ _
-      | Tfunction _ _ _ =>
-          do m' <- store m b ph Scalar;
-          OK (m', next_extern, next_tag, shr_map, [])
-      | Tbox ty' _ | Treference ty' Mutable _ =>
-          (* allocate an external block for ty' *)
-          do (b', m1) <- alloc_external_block ce m ty' next_extern;
+  | Tbox ty' _
+  | Treference ty' Mutable _ =>
+      (* allocate an external block for ty' *)
+      do (b', m1) <- alloc_external_block ce m ty' next_extern;
+      (* update the borrow tree in b' *)
+      do (ap, m2) <- create_reference_from_owner Mutable b' [] (Textern next_tag) m1;
+      (* the abstract pointer is (b', [], Textern next_tag) *)
+      let av := Ptr (Aptrs.singleton ap) in
+      do m3 <- store m2 b ph av;
+      (* initialize b' *)
+      OK (m3, Pos.succ next_extern, Pos.succ next_tag, shr_map, [(b', ty')])
+  | Treference ty' Immutable _ =>
+      (* if [&ty'] has appeared and has been allocated *)
+      match TyMap.get ty' shr_map with
+      | Some b' =>
           (* update the borrow tree in b' *)
-          do (ap, m2) <- create_reference_from_owner Mutable b' [] (Textern next_tag) m1;
-          (* the abstract pointer is (b', [], Textern next_tag) *)
-          let av := Ptr (Aptrs.singleton ap) in
-          do m3 <- store m2 b ph av;
-          (* return (b', ty') *)
-          OK (m3, Pos.succ next_extern, Pos.succ next_tag, shr_map, [b'])
-      | Treference ty' Immutable _ =>
-          (* if [&ty'] has appeared and has been allocated *)
-          match TyMap.get ty' shr_map with
-          | Some b' =>
-              (* update the borrow tree in b' *)
-              (** TODO: here we do not consider annoated origins and
+          (** TODO: here we do not consider annoated origins and
               there subset relation, so we just push tag on top of the
               onwer. If we have t1 ⊆ t2 then we can push t2 on top of
               t1 *)
-              do (ap, m') <- create_reference_from_owner Immutable b' [] (Textern next_tag) m;
-              let av := Ptr (Aptrs.singleton ap) in
-              do m'' <- store m' b ph av;
-              (** FIXME: do we need to update the contents in b'? I
+          do (ap, m') <- create_reference_from_owner Immutable b' [] (Textern next_tag) m;
+          let av := Ptr (Aptrs.singleton ap) in
+          do m'' <- store m' b ph av;
+          (** FIXME: do we need to update the contents in b'? I
               think we do not need to do that because b' must be
               already initialized *)
-              OK (m'', next_extern, Pos.succ next_tag, shr_map, [])
-          | None =>
-              (* allocate an external block for ty' *)
-              do (b', m1) <- alloc_external_block ce m ty' next_extern;
-              (* update the borrow tree in b' *)
-              do (ap, m2) <- create_reference_from_owner Immutable b' [] (Textern next_tag) m1;
-              (* the abstract pointer is (b', [], Textern next_tag) *)
-              let av := Ptr (Aptrs.singleton ap) in
-              do m3 <- store m2 b ph av;
-              (* return (b', ty') and update the share map. But in
-              this moment b' is not initialized and we add b' to
-              shr_map. Is it correct? *)
-              OK (m3, Pos.succ next_extern, Pos.succ next_tag, (TyMap.set ty' b'), [b'])
-          end
-      | Tstruct id _
-      | Tvariant id _ =>
-          match ce!id with
-          | Some co =>
-              
-  
-
-
-(** Deeply initialize an abstract block [b] for a function parameter
-with type [ty] *)
-
-Fixpoint deep_initialize (shr_map: TyMap.t (option ablock)) (rec_map: TyMap.t (option aval)) (ty: type) (b: ablock) (m: amem) (next_extern: positive) (next_tag: positive) : res (amem * (positive * positive) * TyMap.t (option ablock)) :=
-  (* check whether ty is a recursive type and is associated with an
-  initial abstract value *)
-  match TyMap.get ty rec_map with
-  | Some v =>
-      (* use v to initialize b *)
-      do m' <- store m b [] v;
-      OK (m', (next_extern, next_tag))
-  | None =>       
-      match ty with
-      | Tunit
-      | Tint _ _ _
-      | Tlong _ _
-      | Tfloat _ _
-      | Tfunction _ _ _ =>
-          store m b [] Scalar
-      | Tbox ty' _ | Treference ty' Mutable _ =>
+          OK (m'', next_extern, Pos.succ next_tag, shr_map, [])
+      | None =>
           (* allocate an external block for ty' *)
           do (b', m1) <- alloc_external_block ce m ty' next_extern;
           (* update the borrow tree in b' *)
-          do (ap, m2) <- create_reference_from_owner Mutable b' [] (Textern next_tag) m1;
+          do (ap, m2) <- create_reference_from_owner Immutable b' ph (Textern next_tag) m1;
           (* the abstract pointer is (b', [], Textern next_tag) *)
           let av := Ptr (Aptrs.singleton ap) in
           do m3 <- store m2 b [] av;
-          (* initialize b' *)
-          deep_initialize shr_map rec_map ty' b' m3 (Pos.succ next_extern) (Pos.succ next_tag)
-      | Treference ty' Immutable _ =>
-          (* if [&ty'] has appeared and has been allocated *)
-          match TyMap.get ty' shr_map with
-          | Some b' =>
-              (* update the borrow tree in b' *)
-              (** TODO: here we do not consider annoated origins and
-              there subset relation, so we just push tag on top of the
-              onwer. If we have t1 ⊆ t2 then we can push t2 on top of
-              t1 *)
-              do (ap, m') <- create_reference_from_owner Immutable b' [] (Textern next_tag) m;
-              let av := Ptr (Aptrs.singleton ap) in
-              do m'' <- store m' b [] av;
-              (** FIXME: do we need to update the contents in b'? I
-              think we do not need to do that because b' must be
-              already initialized *)
-              OK (m'', (next_extern, Pos.succ next_tag), shr_map)
-          | None =>
-              (* allocate an external block for ty' *)
-              do (b', m1) <- alloc_external_block ce m ty' next_extern;
-              (* update the borrow tree in b' *)
-              do (ap, m2) <- create_reference_from_owner Immutable b' [] (Textern next_tag) m1;
-              (* the abstract pointer is (b', [], Textern next_tag) *)
-              let av := Ptr (Aptrs.singleton ap) in
-              do m3 <- store m2 b [] av;
-              (* initialize b', remember to use a new shr_map *)              
-              deep_initialize (TyMap.set ty' (Some b')) rec_map ty' b' m3 (Pos.succ next_extern) (Pos.succ next_tag)
-          end
-      | Tstruct id _ =>
-          
+          (* initialize b', remember to use a new shr_map *)              
+          OK (m3, Pos.succ next_extern, Pos.succ next_tag, TyMap.set ty' (Some b') shr_map, [(b', ty')])
+      end
+  | Tstruct id _ =>
+      match @get_composite ce id with
+      | co_some _ i co P =>
+          let f acc '(Member_plain fid ty') :=
+            do r1 <- acc;              
+            let '(m1, next_extern1, next_tag1, shr_map1, l1) := r1 in
+            (* shallow initialize this field *)
+            do r2 <- rec (PTree.remove i ce) (PTree_Properties.cardinal_remove P) shr_map ty' b (ph ++ [Rfield fid]) m1 next_extern1 next_tag1;
+            let '(m2, next_extern2, next_tag2, shr_map2, l2) := r2 in
+            OK (m2, next_extern2, next_tag2, shr_map2, l1 ++ l2) in
+          fold_left f co.(co_members) (OK (m, next_extern, next_tag, shr_map, []))
+      | co_none _ =>
+          Error [CTX id; MSG ": cannot get this composite type, maybe it is used in a wrong recursive manner? (shallow initialize)"]
+      end
+  | _ => Error [MSG "TODO (shallow initialize)"]
+  end.          
+
+End SHALLOW_INIT.
+
+Import Wfsimpl.
+
+Definition shallow_initialize ce : t :=
+  Fixm (@PTree_Properties.cardinal composite) shallow_initialize' ce.
+
+Section COMPOSITE_ENV.
+
+Variable ce: composite_env.  
+
+(** Deeply initialize an abstract block [b] for a function parameter
+with type [ty]. Do not know how to specify the recursive parameters *)
+
+Fixpoint deep_initialize (fuel: nat) (shr_map: TyMap.t (option ablock)) (rec_map: TyMap.t (option aval)) (ty: type) (b: ablock) (m: amem) (next_extern: positive) (next_tag: positive) : res (amem * positive * positive * TyMap.t (option ablock)) :=
+  match fuel with
+  | O => Error [MSG "Running out of fuel (deep_initialize)"]
+  | S fuel' =>
+      (* check whether ty is a recursive type and is associated with an
+         initial abstract value *)
+      match TyMap.get ty rec_map with
+      | Some v =>
+          (* use v to initialize b *)
+          do m' <- store m b [] v;
+          OK (m', next_extern, next_tag, shr_map)
+      | None =>       
+          (* invoke shallow initialize *)
+          do r <- shallow_initialize ce shr_map ty b [] m next_extern next_tag;
+          let '(m1, next_extern1, next_tag1, shr_map1, l) := r in
+          (* update the recursive map if [ty] is a composite *)
+          do recv <- load m1 b [];
+          let rec_map1 := match ty with
+                          | Tstruct _ _
+                          | Tvariant _ _ => TyMap.set ty (Some recv) rec_map
+                          | _ => rec_map
+                          end in
+          (* recursively deep initialize the (b, ty) in l *)
+          let f acc '(b, ty) :=
+            do r1 <- acc;
+            let '(m1, next_extern1, next_tag1, shr_map1) := r1 in
+            deep_initialize fuel' shr_map1 rec_map1 ty b m1 next_extern1 next_tag1 in
+          fold_left f l (OK (m1, next_extern1, next_tag1, shr_map1))
+      end
+  end.
+            
+Definition deep_fuel := 100%nat.             
+
+
+(* initialize the parameters *)
+
+Definition init_param (m: amem) (param: ident * type) (shr_map: TyMap.t (option ablock)) (next_extern: positive) (next_tag: positive) : res (amem * positive * positive * TyMap.t (option ablock)) :=
+  let (id, ty) := param in
+  (* allocate a stack block for param so that we can use id to get the
+  content of this parameter *)
+  do (b, m') <- alloc_stack_block ce m ty id;
+  (* deep initialize this parameter *)
+  deep_initialize deep_fuel shr_map (TyMap.init None) ty b m' next_extern next_tag.
+
+Definition init_params_acc acc (param: ident * type) :=
+  do r1 <- acc;
+  let '(m1, next_extern1, next_tag1, shr_map1) := r1 in
+  init_param m1 param shr_map1 next_extern1 next_tag1.
+
+Definition init_params (m: amem) (params: list (ident * type)) : res amem :=
+  do r <- fold_left init_params_acc params (OK (m, 1%positive, 1%positive, TyMap.init None));
+  let '(m', _, _, _) := r in
+  OK m'.
 
 
 (** transfer function (place, pure expression, expression and statement *)
