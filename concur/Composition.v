6c2c65a55f917e510b006fb2266dcefce626632b
Ling Zhang <ling.zhang@sjtu.edu.cn>
2024-08-02 16:51:40 +0800

    added some lemmas

--- concur/Composition.v
+++ concur/Composition.v
@@ -45,8 +45,6 @@ Unset Program Cases.
 *)
 (** lemmas about [wt_c] *)
 
-Search invariant callconv.
-
 Program Coercion cc_inv {li : language_interface} (I : invariant li) : GS.callconv li li :=
   {|
     GS.ccworld := inv_world I;
@@ -65,7 +63,6 @@ Qed.
 
 Infix "@" := GS.cc_compose (at level 30, right associativity).
 
-Search cctrans.
 
 Inductive match_wt_1 : (injp_world * (unit * injp_world)) -> (unit * (injp_world * injp_world)) -> Prop :=
 |match_wt_1_intro w1 w2 tt:
@@ -80,9 +77,12 @@ Proof.
     destruct H0 as [q1' [Hqi [q2 [Hq1 Hq2]]]]. inv Hqi. rename q1' into q1.
     exists (se2, (w1,(se2,((se2,sig) ,w2)))). intuition auto.
     + constructor. auto. constructor. constructor. constructor. auto.
-    + exists q2. constructor. auto. exists q2. constructor. simpl. econstructor.
-      admit. (*should be correct, can be found from old proofs*)
-      auto.
+    + inv Hq1. inv Hq2. inv H. simpl in *.
+      edestruct CallConv.has_type_inject_list as (vl2 & Hvl2 & Hvl & Hvl'); eauto.
+      exists (cq vf2 sg vl2 m2). split. constructor; eauto.
+      exists (cq vf2 sg vl2 m2). split. constructor; eauto.
+      constructor; eauto.
+      eapply CallConv.val_lessdef_inject_list_compose; eauto.
     + constructor.
     + destruct wp1' as [w1' [ss w2']]. destruct ss.
       destruct H1 as [ACCE1 [_ ACCE2]]. simpl in ACCE1, ACCE2.
@@ -94,7 +94,8 @@ Proof.
       destruct H3 as [r2' [Hr1 [r2 [Hri Hr2]]]]. inv Hri.
       repeat econstructor; eauto.
       simpl. simpl in H0.
-      admit.
+      inv Hr1.
+      eapply val_has_type_inject; eauto.
   - red. intros.  rename se2 into se3. rename q2 into q3.
     simpl in wp1. rename wp2 into xxx.
     destruct wp1 as [wp1 [ss wp2]]. destruct ss. inv H2. simpl in w1.
@@ -105,21 +106,30 @@ Proof.
     exists (se1,((se1,sig),(se2,(w1,w2)) )). intuition auto.
     + repeat constructor; auto.
     + repeat constructor; auto.
-    + exists q1. constructor. admit.
+    + exists q1. constructor.
+      inv Hq1. inv H. constructor. simpl in *. split; auto.
+      eapply val_has_type_list_inject; eauto.
       exists q2. constructor; auto.
     + destruct wp2' as [ss [wp1' wp2']]. destruct ss.
       destruct H0 as [x [E1 E2]]. simpl in x,E1,E2. rename r2 into r3.
       destruct H1 as [r1' [Hri [r2 [Hr1 Hr2]]]]. inv Hri.
       exists (wp1', (tt, wp2')). intuition auto.
       repeat constructor; auto.
-      exists r2. split; auto. exists r2. split; auto. admit.
+      inv Hr1. simpl in *.
+      exists (cr (Val.ensure_type vres2 (proj_sig_res sig)) m2').
+      split. constructor; eauto.
+      eapply CallConv.has_type_inject; eauto.
+      exists (cr (Val.ensure_type vres2 (proj_sig_res sig)) m2').
+      constructor. constructor. apply Val.ensure_has_type.
+      inv Hr2. econstructor; eauto.
+      eapply Val.inject_ensure_type_l; eauto.
       constructor.
-Admitted.
+Qed.
 
 Inductive match_assoc_1 {A B C : Type}: (A * (B * C)) -> (A * B * C) -> Prop :=
 |match_assoc_1_intro a b c: match_assoc_1 (a,(b,c)) (a,b,c).
 
-Lemma cc_compose_assoc {A B C D}:
+Lemma cc_compose_assoc_1 {A B C D}:
   forall (cc1 : GS.callconv A B) (cc2 : GS.callconv B C) (cc3 : GS.callconv C D),
     cctrans (cc1 @ cc2 @ cc3) ((cc1 @ cc2) @ cc3).
 Proof.
@@ -157,6 +167,54 @@ Proof.
     repeat econstructor; eauto.
     constructor.
 Qed.
+
+Lemma move_wt_ext : cctrans (c_injp @ wt_c @ c_ext) (wt_c @ c_injp @ c_ext).
+Admitted. (*should be the same as above*)
+
+Inductive match_assoc_2 {A B C : Type}: (A * B * C) -> (A * (B * C)) -> Prop :=
+|match_assoc_2_intro a b c: match_assoc_2 (a,b,c) (a,(b,c)).
+
+Lemma cc_compose_assoc_2 {A B C D}:
+  forall (cc1 : GS.callconv A B) (cc2 : GS.callconv B C) (cc3 : GS.callconv C D),
+    cctrans ((cc1 @ cc2) @ cc3) (cc1 @ cc2 @ cc3).
+Proof.
+  econstructor. instantiate (1:= match_assoc_2).
+  - red.
+    intros [seb [w1 [sec [w2 w3]]]]
+      sea sed qa qd.
+    intros (Hseab & Hsebc & Hsecd) (qb & Hqab & qc & Hqbc & Hqcd).
+    exists (sec,((seb,(w1,w2)),w3)).
+    intuition auto.
+    repeat constructor; auto.
+    repeat econstructor; eauto.
+    constructor.
+    destruct wp1' as [[w1' w2'] w3'].
+    rename wp2 into xx.
+    destruct wp1 as [[wp1 wp2] wp3]. inv H.
+    destruct H0 as [[E1 E2] E3]. destruct H1 as [[I1 I2] I3].
+    simpl in E1, E2, E3, I1, I2, I3. destruct H2 as [rc [[rb [Hr1 Hr2]] Hr3]].
+    exists (w1', (w2', w3')). intuition auto.
+    repeat constructor; auto.
+    repeat constructor; auto.
+    repeat econstructor; eauto.
+  - red.
+    intros [[w1 w2] w3] [wp1 [wp2 wp3]] [sec [[seb [w1' w2']] w3']] sea sed qa qd.
+    intros ((Hseab & Hsebc) & Hsecd) (qb & (Hqab & qc & Hqbc) & Hqcd) [[I1 I2] I3] Hm.
+    simpl in I1, I2, I3. inv Hm.
+    exists (seb, (w1', (sec, (w2', w3')))). intuition auto.
+    repeat constructor; auto.
+    repeat econstructor; eauto.
+    repeat econstructor; eauto.
+    simpl in H. destruct wp2' as [w1'' [w2'' w3'']].
+    destruct H as [E1 [E2 E3]]. simpl in E1, E2, E3.
+    exists ((w1'', w2''), w3''). intuition auto.
+    repeat constructor; auto.
+    destruct H0 as [rb [Hr1 [rc [Hr2 Hr3]]]].
+    repeat econstructor; eauto.
+    constructor.
+Qed.
+
+
 
 
 
