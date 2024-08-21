bb219215d40e90c4a22d789580f420e1e62cf959
meng0181 <meng0181@umn.edu>
2024-08-22 00:57:17 +0800

    small framework of elaboratedrop proof.

--- rustfrontend/ElaborateDropProof.v
+++ rustfrontend/ElaborateDropProof.v
@@ -18,9 +18,24 @@ Local Open Scope error_monad_scope.
 Local Open Scope gensym_monad_scope.
 
 Record match_prog (p tp: RustIR.program) : Prop := {
-   match_prog_skel:
-    erase_program tp = erase_program p
+  match_prog_main:
+    tp.(prog_main) = p.(prog_main);
+  match_prog_public:
+    tp.(prog_public) = p.(prog_public);
+  match_prog_skel:
+    erase_program tp = erase_program p;
+  match_prog_defs:
+    list_norepet (prog_defs_names p)
 }.
+
+Lemma match_transf_program: forall p tp,
+    transl_program p = OK tp ->
+    match_prog p tp.
+Proof.
+  intros. unfold transl_program in H. monadInv H. unfold transform_partial_program in EQ.
+  destruct p. simpl in *. unfold transform_partial_program2 in EQ. 
+Admitted. 
+
 
 Section MATCH_PROGRAMS.
 Variable p: RustIR.program.
@@ -58,21 +73,19 @@ Variable tprog: RustIR.program.
 (* Let build_ctx := build_clgen_env p tp. *)
 
 
-Variable w: inj_world. 
 
 Variable se: Genv.symtbl.
 Variable tse: Genv.symtbl.
-Variable dropflags: PTree.t (list (place * ident)).
+(* Variable dropflags: PTree.t (list (place * ident)). *)
 
 Let ge := globalenv se prog.
 Let tge := globalenv tse tprog.
 
 
-Inductive match_states: state -> RustIRsem.state -> Prop :=
+Inductive match_states: state -> RustIRsem.state -> Prop := 
   | match_regular_state:
-      forall f s k e own m tf ts tk te tm j
+      forall f s k e own m tf ts tk te tm j dropflags
       (TRS: transl_stmt dropflags s = ts)
-      (INCR: inj_incr w (injw j (Mem.support m) (Mem.support tm)))
       (MINJ: Mem.inject j m tm),
       match_states (State f s k e own m) (RustIRsem.State tf ts tk te tm). 
 
@@ -85,18 +98,43 @@ Proof.
   - eexists. split. eapply plus_one. 
 Admitted. 
 
+Lemma transf_initial_states q:
+  forall S1, initial_state ge q S1 ->
+  exists S2, RustIRsem.initial_state tge q S2 /\ match_states S1 S2.
+Proof.
+Admitted. 
+
+Lemma transf_final_states:
+  forall S1 S2 r, match_states S1 S2 -> final_state S1 r -> RustIRsem.final_state S2 r.
+Proof.
+Admitted. 
+
+Lemma transf_external:
+  forall S R q, match_states S R -> at_external ge S q ->
+  RustIRsem.at_external tge R q /\
+  forall r S', after_external S r S' ->
+  exists R', RustIRsem.after_external R r R' /\ match_states S' R'.
+Proof.
+  intros S R q HSR Hq. destruct Hq; inv HSR.
+Qed. 
+
+Lemma transf_fundef_internal: 
+forall q se2,
+Genv.is_internal (Genv.globalenv se2 tprog) (cq_vf q) =
+Genv.is_internal (Genv.globalenv se2 prog) (cq_vf q). 
+Admitted. 
 
 End PRESERVATION.
 
 Theorem transl_program_correct prog tprog:
    match_prog prog tprog ->
-   forward_simulation (cc_c inj) (cc_c inj) (semantics prog) (RustIRsem.semantics tprog).
+   forward_simulation (cc_id) (cc_id) (semantics prog) (RustIRsem.semantics tprog).
 Proof.
     fsim eapply forward_simulation_plus; simpl in *. 
     - inv MATCH. simpl. auto. 
-    - simpl. intros. destruct Hse, H. simpl. exploit is_internal_match; eauto. 
-    - admit. 
-    - admit. 
-    - admit. 
-    - eapply step_simulation; auto.   
-    Admitted.   
+    - intros. inv H. eapply transf_fundef_internal; eauto. 
+    - intros. inv H. eapply transf_initial_states. eauto.  
+    - intros. exploit transf_final_states; eauto. 
+    - intros. edestruct transf_external; eauto. exists tt, q1. intuition subst; eauto.
+    - eauto using step_simulation.
+Admitted. 
