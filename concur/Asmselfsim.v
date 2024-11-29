07ccc29bdea04c717388291af0e530f8e976f960
Ling Zhang <ling.zhang@sjtu.edu.cn>
2024-11-29 14:27:01 +0800

    added Asmselfsim proof, leaving several lemmas

--- concur/Asmselfsim.v
+++ concur/Asmselfsim.v
@@ -114,6 +114,15 @@ Proof.
   inv H1; congruence.
 Qed.
 
+Lemma regset_lessdef_int : forall rs1 rs2 r i,
+    regset_lessdef rs1 rs2 ->
+    rs1 r = Vint i ->
+    rs2 r = Vint i.
+Proof.
+  intros. generalize (H r). intro.
+  inv H1; congruence.
+Qed.
+
 Lemma regset_lessdef_set_pair : forall rs1 rs2 p v1 v2,
     regset_lessdef rs1 rs2 ->
     Val.lessdef v1 v2 ->
@@ -145,6 +154,44 @@ Proof.
     inv H0; constructor.
 Qed.
 
+Lemma regset_lessdef_compare_ints : forall rs1 rs2 m1 m2 v1 v2 v11 v22,
+    Mem.extends m1 m2 ->
+    Val.lessdef v1 v2 ->
+    Val.lessdef v11 v22 ->
+    regset_lessdef rs1 rs2 ->
+    regset_lessdef (compare_ints v1 v11 rs1 m1) (compare_ints v2 v22 rs2 m2).
+Proof.
+Admitted.
+
+Lemma regset_lessdef_compare_longs : forall rs1 rs2 m1 m2 v1 v2 v11 v22,
+    Mem.extends m1 m2 ->
+    Val.lessdef v1 v2 ->
+    Val.lessdef v11 v22 ->
+    regset_lessdef rs1 rs2 ->
+    regset_lessdef (compare_longs v1 v11 rs1 m1) (compare_longs v2 v22 rs2 m2).
+Proof.
+Admitted.
+
+Lemma regset_lessdef_compare_floats : forall rs1 rs2 v1 v2 v11 v22,
+    regset_lessdef rs1 rs2 ->
+       Val.lessdef v1 v2 ->
+    Val.lessdef v11 v22 ->
+    regset_lessdef (compare_floats v1 v11 rs1) (compare_floats v2 v22 rs2).
+Proof.
+Admitted.
+
+Lemma regset_lessdef_compare_floats32 : forall rs1 rs2 v1 v2 v11 v22,
+    regset_lessdef rs1 rs2 ->
+       Val.lessdef v1 v2 ->
+    Val.lessdef v11 v22 ->
+    regset_lessdef (compare_floats32 v1 v11 rs1) (compare_floats32 v2 v22 rs2).
+Proof.
+Admitted.
+
+Local Hint Resolve Val.zero_ext_lessdef Val.sign_ext_lessdef Val.loword_lessdef
+  Val.singleoffloat_lessdef
+  : val.
+
 Ltac rs_lessdef :=
   match goal with
   | [ |- regset_lessdef (nextinstr_nf _ ) (nextinstr_nf _) ] =>
@@ -158,9 +205,19 @@ Ltac rs_lessdef :=
   | [ |- regset_lessdef (_ # _ <- _ ) (_ # _ <- _) ] =>
       eapply regset_lessdef_set; eauto
   | [ |- regset_lessdef (set_pair _ _ _ ) (set_pair _ _ _) ] =>
-      eapply regset_lessdef_set_pair; eauto
+      eapply regset_lessdef_set_pair; eauto 
   | [ |- regset_lessdef (set_res _ _ _ ) (set_res _ _ _) ] =>
       eapply regset_lessdef_set_res; eauto
+  | [ |- regset_lessdef (compare_ints _ _ _ _) (compare_ints _ _ _ _) ] =>
+      eapply regset_lessdef_compare_ints; eauto
+  | [ |- regset_lessdef (compare_longs _ _ _ _) (compare_longs _ _ _ _) ] =>
+      eapply regset_lessdef_compare_longs; eauto
+  | [ |- regset_lessdef (compare_floats _ _ _) (compare_floats _ _ _) ] =>
+      eapply regset_lessdef_compare_floats; eauto
+  | [ |- regset_lessdef (compare_floats32 _ _ _) (compare_floats32 _ _ _) ] =>
+      eapply regset_lessdef_compare_floats32; eauto
+  | [ |- Val.lessdef _ _ ] =>
+      eauto with val
   end.
 
 Ltac rs_lessdefs := repeat rs_lessdef.
@@ -168,17 +225,87 @@ Ltac rs_lessdefs := repeat rs_lessdef.
 
 (** For exec_load *)
 
+
+Lemma mull_lessdef :
+forall v1 v1' v2 v2' : val,
+  Val.lessdef v1 v1' -> Val.lessdef v2 v2' -> Val.lessdef (Val.mull v1 v2) (Val.mull v1' v2').
+Proof.
+  intros. destruct v1; inv H; destruct v2; inv H0; constructor.
+Qed.
+
+Lemma mul_lessdef :
+forall v1 v1' v2 v2' : val,
+  Val.lessdef v1 v1' -> Val.lessdef v2 v2' -> Val.lessdef (Val.mul v1 v2) (Val.mul v1' v2').
+Proof.
+  intros. destruct v1; inv H; destruct v2; inv H0; constructor.
+Qed.
+
 Lemma eval_addrmode_lessdef : forall rs1 rs2 a se,
     regset_lessdef rs1 rs2 ->
     Val.lessdef (eval_addrmode se a rs1) (eval_addrmode se a rs2).
 Proof.
-Admitted.
+  intros. unfold eval_addrmode.
+  replace Archi.ptr64 with true by reflexivity.
+  destruct a. simpl. repeat destr; subst;
+    repeat eapply Val.addl_lessdef; eauto;
+    eapply mull_lessdef; eauto.
+Qed.
 
 
 (** For exec_store *)
 
 
 
+Lemma exec_load_trans : forall chunk m1 a rs1 rd m1' rs1' m2 rs2 flag (Hm: Mem.extends m1 m2),
+    exec_load se chunk m1 a rs1 rd = Next' rs1' m1' flag ->
+    regset_lessdef rs1 rs2 ->
+    exists m2' rs2' Hm',
+      exec_load se chunk m2 a rs2 rd = Next' rs2' m2' flag /\
+        regset_lessdef rs1' rs2' /\
+        ext_acci (extw m1 m2 Hm) (extw m1' m2' Hm').
+Proof.
+  intros.
+  unfold exec_load in *. destr_in H. inv H.
+  exploit eval_addrmode_lessdef. eauto. intro.
+  exploit Mem.loadv_extends; eauto. intros [v' [Hl Hv]].
+  exists m2. eexists. exists Hm. rewrite Hl. split. reflexivity.
+  split.
+  rs_lessdefs. reflexivity.
+Qed.
+
+Lemma exec_store_trans : forall chunk m1 a r rs1 l rs1' m1' flag m2 rs2 (Hm:Mem.extends m1 m2),
+    exec_store se chunk m1 a rs1 r l = Next' rs1' m1' flag ->
+    regset_lessdef rs1 rs2 ->
+    exists m2' rs2' Hm',
+      exec_store se chunk m2 a rs2 r l = Next' rs2' m2' flag /\
+        regset_lessdef rs1' rs2' /\
+        ext_acci (extw m1 m2 Hm) (extw m1' m2' Hm').
+Proof.
+  intros. unfold exec_store in *. destr_in H. inv H.
+  exploit eval_addrmode_lessdef. eauto. intro.
+  exploit Mem.storev_extends; eauto. intros [m2' [Hs He]].
+  exploit ext_acci_storev. apply Heqo. apply Hs. eauto. intro Hci.
+  exists m2'. eexists. exists He. rewrite Hs. split. reflexivity.
+  split. rs_lessdefs. eauto.
+Qed.
+
+Lemma eval_testcond_lessdef : forall c rs1 rs2,
+    regset_lessdef rs1 rs2 ->
+    option_le eq (eval_testcond c rs1) (eval_testcond c rs2).
+Proof.
+  Ltac solve1 :=  erewrite regset_lessdef_int; eauto; constructor; eauto.
+  Ltac solve2 := destr; erewrite regset_lessdef_int; try constructor;
+      try erewrite regset_lessdef_int; eauto;
+    constructor; eauto.
+  intros. destruct c; simpl; destr; try constructor; try solve1; try solve2.
+  - generalize (H OF). intro HOF. rewrite Heqv in HOF. inv HOF.
+    destr; try constructor. solve2.
+  - generalize (H OF). intro HOF. rewrite Heqv in HOF. inv HOF.
+    destr; try constructor. solve2.
+Qed.
+
+
+        
 Lemma step_correct:
   forall s1 t s2, step init_sup ge s1 t s2 ->
   forall wp s1' (MS : match_states wp s1 s1'),
@@ -186,9 +313,8 @@ Lemma step_correct:
 Proof.
   induction 1; intros; inv MS.
   - (* internal steps *)
-    admit.
-    (*
     (* only for trivial instrs *)
+
     Ltac solve_exec_instr1 :=
       eexists; split;
       [econstructor; eauto using regset_lessdef_ptr;
@@ -200,77 +326,121 @@ Proof.
       [econstructor; eauto using regset_lessdef_ptr;
        try econstructor; eauto |
         econstructor; eauto; try rs_lessdefs ].
-    destruct i; inv EXEC; try solve_exec_instr1.
-    + (*exec_load*)
-      unfold exec_load in *. destr_in H1. inv H1.
-      exploit eval_addrmode_lessdef. eauto. intro.
-      exploit Mem.loadv_extends; eauto. intros [v' [Hl Hv]].
-      solve_exec_instr. simpl. unfold exec_load. rewrite Hl. reflexivity.
-      rs_lessdefs.
-    + (*exec_load*)
-      unfold exec_load in *. destr_in H1. inv H1.
-      exploit eval_addrmode_lessdef. eauto. intro.
-      exploit Mem.loadv_extends; eauto. intros [v' [Hl Hv]].
-      solve_exec_instr. simpl. unfold exec_load. rewrite Hl. reflexivity.
-      rs_lessdefs.
-    + (*exec_store*)
-      unfold exec_store in *. destr_in H1. inv H1.
-      exploit eval_addrmode_lessdef. eauto. intro.
-      exploit Mem.storev_extends; eauto. intros [m'1 [Hs He]].
-      exploit ext_acci_storev. apply Heqo. apply Hs. eauto. intro ACI1.
-      solve_exec_instr. simpl. unfold exec_store. rewrite Hs. reflexivity.
-      rs_lessdefs. instantiate (1:= He). etransitivity; eauto. etransitivity; eauto.
-    + (*exec_store*)
-      unfold exec_store in *. destr_in H1. inv H1.
-      exploit eval_addrmode_lessdef. eauto. intro.
-      exploit Mem.storev_extends; eauto. intros [m'1 [Hs He]].
-      exploit ext_acci_storev. apply Heqo. apply Hs. eauto. intro ACI1.
-      solve_exec_instr. simpl. unfold exec_store. rewrite Hs. reflexivity.
-      rs_lessdefs. instantiate (1:= He). etransitivity; eauto. etransitivity; eauto.
-    + unfold exec_load in *. destr_in H1. inv H1.
-      exploit eval_addrmode_lessdef. eauto. intro.
-      exploit Mem.loadv_extends; eauto. intros [v' [Hl Hv]].
-      solve_exec_instr. simpl. unfold exec_load. rewrite Hl. reflexivity.
-      rs_lessdefs.
-    + unfold exec_store in *. destr_in H1. inv H1.
-      exploit eval_addrmode_lessdef. eauto. intro.
-      exploit Mem.storev_extends; eauto. intros [m'1 [Hs He]].
-      exploit ext_acci_storev. apply Heqo. apply Hs. eauto. intro ACI1.
-      solve_exec_instr. simpl. unfold exec_store. rewrite Hs. reflexivity.
-      rs_lessdefs. instantiate (1:= He). etransitivity; eauto. etransitivity; eauto.
-    + admit.
-    + 
-    + solve_exec_instr.
-    + solve_exec_instr.
-    + (*exec_load*)
-      unfold exec_load in *. destr_in H1. inv H1.
-      exploit eval_addrmode_lessdef. eauto. intro.
-      exploit Mem.loadv_extends; eauto. intros [v' [Hl Hv]].
-      solve_exec_instr. simpl. unfold exec_load. rewrite Hl. reflexivity.
-      rs_lessdefs.
-    + admit.
-    + solve_exec_instr.
-    + admit.
-    + admit.
-    + admit.
-    + admit.
-    + admit.
-    + admit.
-    + admit.
-    + admit.
-    + solve_exec_instr. admit.
-    + admit.
-    + solve_exec_instr. admit.
-    + admit.
-    + solve_exec_instr. admit.
-    + admit.
-    + solve_exec_instr. admit.
-    + admit.
-    + solve_exec_instr.
-      generalize (RLD rs0). intro. inv H0; constructor.
-    + solve_exec_instr. admit.
-    + solve_exec_instr. 
-     *)
+     Ltac solve_exec_load :=
+      exploit exec_load_trans; eauto;
+      intros (m2' & rs2' & Hm' & Hl & Hrs & Haci);
+      solve_exec_instr; etransitivity; eauto.
+     Ltac solve_exec_store :=
+     exploit exec_store_trans; eauto;
+      intros (m2' & rs2' & Hm' & Hs & Hrs & Haci);
+     solve_exec_instr; etransitivity; eauto.
+     Ltac solve_using_rlib := apply val_inject_id; repeat rstep; apply val_inject_id; eauto.     
+     destruct i; inv EXEC;
+       try solve_exec_instr1;
+       try solve_exec_load;
+       try solve_exec_store;
+       try solve_using_rlib.
+    + destruct a; simpl. repeat destr; subst; repeat apply Val.add_lessdef; eauto;
+        repeat apply mul_lessdef; eauto.
+    + destruct a; simpl. repeat destr; subst; repeat apply Val.addl_lessdef; eauto;
+        repeat apply mull_lessdef; eauto.
+    + (* division *)
+      repeat destr_in H1. solve_exec_instr.
+      simpl.
+      generalize (RLD RDX). intro Hrdx. rewrite Heqv in Hrdx. inv Hrdx.
+      generalize (RLD RAX). intro Hrax. rewrite Heqv0 in Hrax. inv Hrax.
+      generalize (RLD r1). intro Hr1. rewrite Heqv1 in Hr1. inv Hr1.
+      rewrite Heqo. reflexivity. rs_lessdefs.
+    + repeat destr_in H1. solve_exec_instr.
+      simpl.
+      generalize (RLD RDX). intro Hrdx. rewrite Heqv in Hrdx. inv Hrdx.
+      generalize (RLD RAX). intro Hrax. rewrite Heqv0 in Hrax. inv Hrax.
+      generalize (RLD r1). intro Hr1. rewrite Heqv1 in Hr1. inv Hr1.
+      rewrite Heqo. reflexivity. rs_lessdefs.
+    + repeat destr_in H1. solve_exec_instr.
+      simpl.
+      generalize (RLD RDX). intro Hrdx. rewrite Heqv in Hrdx. inv Hrdx.
+      generalize (RLD RAX). intro Hrax. rewrite Heqv0 in Hrax. inv Hrax.
+      generalize (RLD r1). intro Hr1. rewrite Heqv1 in Hr1. inv Hr1.
+      rewrite Heqo. reflexivity. rs_lessdefs.
+    + repeat destr_in H1. solve_exec_instr.
+      simpl.
+      generalize (RLD RDX). intro Hrdx. rewrite Heqv in Hrdx. inv Hrdx.
+      generalize (RLD RAX). intro Hrax. rewrite Heqv0 in Hrax. inv Hrax.
+      generalize (RLD r1). intro Hr1. rewrite Heqv1 in Hr1. inv Hr1.
+      rewrite Heqo. reflexivity. rs_lessdefs.
+    + exploit eval_testcond_lessdef; eauto.
+      instantiate (1:= c).
+      intro Hcond. inv Hcond. destr; rs_lessdef. constructor.
+    + apply val_inject_id. repeat rstep.
+      instantiate (2:= ext).
+      instantiate (1:= extw m' m'0 Hm).
+      red. red. intros. simpl. apply val_inject_id; eauto.
+    + unfold goto_label in H1. repeat destr_in H1; subst. inv H.
+      solve_exec_instr. simpl. unfold goto_label. rewrite Heqo.
+      erewrite regset_lessdef_ptr; eauto. rs_lessdefs.
+    + exploit eval_testcond_lessdef; eauto. instantiate (1:=c).
+      intro Hcond. inv Hcond. rewrite <- H0 in H1. destr_in H1. unfold goto_label in H1.
+      repeat destr_in H1. inv H. solve_exec_instr. simpl. rewrite <- H2. unfold goto_label.
+      rewrite Heqo. erewrite regset_lessdef_ptr; eauto. rs_lessdefs.
+      inv H1.
+      solve_exec_instr. simpl. rewrite <- H2. reflexivity. rs_lessdef.
+      rewrite <- H2 in H1. inv H1.
+    + exploit eval_testcond_lessdef; eauto. instantiate (1:=c1). intro Hc1.
+      exploit eval_testcond_lessdef; eauto. instantiate (1:=c2). intro Hc2.
+      inv Hc1; inv Hc2.
+      -- rewrite <- H0 in H1. rewrite <- H3 in H1. unfold goto_label in H1.
+         repeat destr_in H1; inv H; solve_exec_instr; simpl; unfold goto_label.
+         ++ rewrite <- H2. rewrite <- H4. rewrite Heqo. erewrite regset_lessdef_ptr; eauto.
+         ++ rs_lessdefs.
+         ++ rewrite <- H2. rewrite <- H4. reflexivity.
+         ++ rs_lessdef.
+         ++ rewrite <- H2. rewrite <- H4. reflexivity.
+         ++ rs_lessdef.
+      -- rewrite <- H0 in H1. rewrite <- H4 in H1. destr_in H1.
+      -- rewrite <- H2 in H1. inv H1.
+      -- rewrite <- H2 in H1. inv H1.
+    + repeat destr_in H1. unfold goto_label in H2. repeat destr_in H2.
+      rewrite Pregmap.gso in Heqv0; try congruence.
+      rewrite Pregmap.gso in Heqv0; try congruence. rewrite H in Heqv0. inv Heqv0.
+      solve_exec_instr. simpl.
+      generalize (RLD r). intro HH. rewrite Heqv in HH. inv HH.
+      rewrite Heqo. unfold goto_label. rewrite Heqo0. rewrite !Pregmap.gso; try congruence.
+      erewrite regset_lessdef_ptr; eauto. rs_lessdefs.
+    + unfold inner_sp in H1. repeat destr_in H1. destr_in Heqo. inv Heqo. solve_exec_instr. simpl.
+      unfold inner_sp. erewrite regset_lessdef_ptr; eauto. rs_lessdefs.
+    + repeat destr_in H1. exploit Mem.alloc_extends; eauto. reflexivity. reflexivity.
+      intros (m'1 & ALLOC & ME1). exploit ext_acci_alloc. apply Heqp. eauto. intro Haci1.
+      exploit Mem.store_within_extends; eauto.
+      intros (m'2 & STORE1 & ME2). exploit ext_acci_store. apply Heqo. eauto. intro Haci2.
+      exploit Mem.store_within_extends; eauto.
+      intros (m'3 & STORE2 & ME3). exploit ext_acci_store. apply Heqo0. eauto. intro Haci3.
+      assert (HACI: ext_acci (extw m m'0 Hm) (extw m' m'3 ME3)).
+      etransitivity. eauto. instantiate (1:= ME1). etransitivity. eauto.
+      instantiate (1:= ME2). eauto.
+      solve_exec_instr. simpl. rewrite ALLOC, STORE1, STORE2. reflexivity.
+      rs_lessdefs. etransitivity; eauto. etransitivity; eauto.
+    + repeat destr_in H1. 
+      exploit Mem.loadv_extends. apply Hm. apply Heqo. eauto.
+      intros [v' [Hra Vra]].
+      exploit Mem.loadv_extends. apply Hm. apply Heqo0. eauto.
+      intros [v0' [Hlink Vlink]].
+      unfold free' in Heqo1. destr_in Heqo1.
+      --
+        exploit Mem.free_parallel_extends; eauto. intros (m'1 & Hf & He).
+        exploit ext_acci_free. apply Heqo1. eauto. intro Haci.
+        solve_exec_instr. simpl. erewrite !regset_lessdef_ptr; eauto.
+        rewrite Hra, Hlink. unfold free'. rewrite pred_dec_true; eauto. rewrite Hf.
+        reflexivity. rs_lessdefs. instantiate (1:= He).
+        etransitivity; eauto. etransitivity; eauto.
+      -- inv Heqo1.
+        solve_exec_instr. simpl. erewrite !regset_lessdef_ptr; eauto.
+        rewrite Hra, Hlink. unfold free'. rewrite pred_dec_false; eauto.
+        rs_lessdefs.
+    + generalize (RLD rd) (RLD r2). intros H1 H2.
+      destruct (rs rd); inv H1; destruct (rs r2); inv H2; constructor.
+    + generalize (RLD rd) (RLD r2). intros H1 H2.
+      destruct (rs rd); inv H1; destruct (rs r2); inv H2; constructor.
   - (* builtin steps *)
     exploit (eval_builtin_args_rel ext (extw m m'0 Hm)); simpl.
     instantiate (1:= se). eapply Genv.match_stbls_id.
@@ -304,7 +474,7 @@ Proof.
     eauto. apply Heqv. intro. rewrite H0. rewrite ISP. reflexivity.
     econstructor; eauto. rs_lessdefs.
     etransitivity; eauto. etransitivity; eauto.
-Admitted. 
+Qed.
 
 Lemma initial_correct:
   forall q1 q2 st1, GS.match_query asm_ext w q1 q2 -> initial_state ge q1 st1 ->
